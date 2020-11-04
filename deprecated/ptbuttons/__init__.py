3d48d101411578c4c8792b7f560808f2ef0510fb
Jorge Capona <jcapona@gmail.com>
2020-11-04 13:36:21 -0300

    Fix imports on deprecated ptbuttons library

--- deprecated/ptbuttons/__init__.py
+++ deprecated/ptbuttons/__init__.py
@@ -1,4 +1,12 @@
-from pitop.case_buttons import request_client
-print("Note: Use of the 'ptbuttons' package is now deprecated. Please use 'pitop.case_buttons' instead.")
+from pitop.case_buttons import (
+    request_client,
+    PTButton,
+    PTButtons,
+    PTUpButton,
+    PTDownButton,
+    PTSelectButton,
+    PTCancelButton
+)
+print("Note, Use of the 'ptbuttons' package is now deprecated. Please use 'pitop.case_buttons' instead.")
 # TODO
-print("For more information, please see <TODO: URL>")
+print("For more information, please see <TODO, URL>")
