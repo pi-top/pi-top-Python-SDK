623c4ef5b9a17b89528e1ad2c48bd55e8038409a
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2020-11-08 13:31:03 +0000

    Update README (#31)

--- examples/oled/animated_image_once_simple_way.py
+++ examples/oled/animated_image_once_simple_way.py
@@ -1,8 +1,6 @@
-from pitop.oled import PTOLEDDisplay, OLEDImage
+from ptoled import PTOLEDDisplay, OLEDImage
 from time import sleep
 
 ptoled = PTOLEDDisplay()
-ptoled.set_max_fps(10)
-
 image = OLEDImage("/usr/share/pt-project-files/images/rocket.gif")
 ptoled.play_animated_image(image)
