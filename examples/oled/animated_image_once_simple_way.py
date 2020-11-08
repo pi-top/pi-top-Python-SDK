bb5a3fb28a910ba855f39e181f3a979a8cbd53e7
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2020-11-08 13:28:48 +0000

    Revert changes unrelated to Actions

--- examples/oled/animated_image_once_simple_way.py
+++ examples/oled/animated_image_once_simple_way.py
@@ -1,6 +1,8 @@
-from ptoled import PTOLEDDisplay, OLEDImage
+from pitop.oled import PTOLEDDisplay, OLEDImage
 from time import sleep
 
 ptoled = PTOLEDDisplay()
+ptoled.set_max_fps(10)
+
 image = OLEDImage("/usr/share/pt-project-files/images/rocket.gif")
 ptoled.play_animated_image(image)
