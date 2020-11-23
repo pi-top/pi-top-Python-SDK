5a59ee41195c8fe7d1cf4b94567eda1c03c94347
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2020-11-23 13:08:20 +0000

    Fix message

--- .deprecated/ptbuttons/__init__.py
+++ .deprecated/ptbuttons/__init__.py
@@ -4,6 +4,6 @@ from pitop.miniscreen.buttons import UpButton as PTUpButton  # noqa: F401
 from pitop.miniscreen.buttons import DownButton as PTDownButton  # noqa: F401
 from pitop.miniscreen.buttons import SelectButton as PTSelectButton  # noqa: F401
 from pitop.miniscreen.buttons import CancelButton as PTCancelButton  # noqa: F401
-print("Note, Use of the 'ptbuttons' package is now deprecated. Please use 'pitop.case_buttons' instead.")
+print("Note, Use of the 'ptbuttons' package is now deprecated. Please use 'pitop.miniscreen.buttons' instead.")
 # TODO
 print("For more information, please see <TODO, URL>")
