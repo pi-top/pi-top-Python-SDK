d0ebffee6d7e195183c51f0bfdd3294305b03d59
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2020-11-26 12:22:52 +0000

    Initial plan

--- .deprecated/ptoled/__init__.py
+++ .deprecated/ptoled/__init__.py
@@ -1,14 +1,12 @@
 from pitop.miniscreen.oled import (  # noqa: F401
-    get_device_instance,
-    device_reserved,
-    reset_device_instance,
     canvas,
     device_helper,
-    display,
-    fps_regulator,
     oled_image,
     oled_manager,
-    request_client
+)
+from pitop.miniscreen.oled.core import (  # noqa: F401
+    display,
+    fps_regulator,
 )
 from pitop.miniscreen.oled import OLEDDisplay as PTOLEDDisplay  # noqa: F401
 print("Note: Use of the 'ptoled' package is now deprecated. Please use 'pitop.miniscreen.oled' instead.")
--- pitop/miniscreen/oled/__init__.py
+++ pitop/miniscreen/oled/__init__.py
@@ -1,5 +1,6 @@
 from .oled_display import OLEDDisplay  # noqa: F401
 from .oled_image import OLEDImage  # noqa: F401
+# TODO: make internal; expose "get/set control" and "get OLED object"
 from .device_helper import (  # noqa: F401
     get_device_instance,
     device_reserved,
