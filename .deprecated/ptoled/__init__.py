d2647ec0db44bbd6781ab5583945d4095e05a2ed
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2020-12-01 17:45:47 +0000

    Fix import

--- .deprecated/ptoled/__init__.py
+++ .deprecated/ptoled/__init__.py
@@ -7,7 +7,7 @@ from pitop.miniscreen.oled.core import (  # noqa: F401
     display,
     fps_regulator,
 )
-from pitop.miniscreen.oled.controls import (  # noqa: F401
+from pitop.miniscreen.oled.core.controls import (  # noqa: F401
     get_oled_device as get_device_instance,
     oled_device_is_active as device_reserved,
     reset_oled_device as reset_device_instance,
