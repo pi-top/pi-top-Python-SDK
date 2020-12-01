3cc13e9ded81714b0ad6b91cd20fbbde9a588a7f
Jorge Capona <jcapona@gmail.com>
2020-12-01 19:20:41 -0300

    WIP: update OLED CLI arguments

--- pitopcli/oled.py
+++ pitopcli/oled.py
@@ -19,7 +19,11 @@ class OledCLI(CliBaseClass):
             oled = OLED()
             if self.args.force:
                 oled.set_control_to_pi()
-            oled.draw_multiline_text(self.args.text, font_size=self.args.font_size)
+
+            if self.args.oled_subcommand == "draw":
+                oled.draw_multiline_text(self.args.text, font_size=self.args.font_size)
+            else:
+                print("Not implemented yet.")
             sleep(self.args.timeout)
             return 0
         except Exception:
@@ -27,24 +31,49 @@ class OledCLI(CliBaseClass):
 
     @classmethod
     def add_parser_arguments(cls, parser) -> None:
-        parser.add_argument("--force", "-f",
-                            type=int,
-                            help="Force the hub to give control of the OLED to the Pi",
-                            default=False,
-                            )
-        parser.add_argument("--timeout", "-t",
-                            type=int,
-                            help="Set the timeout in seconds",
-                            default=10,
-                            )
-        parser.add_argument("--font-size", "-s",
-                            type=int,
-                            help="Set the font size",
-                            default=20,
-                            )
-        parser.add_argument("text",
-                            help="Set the text to write to screen",
-                            )
+        subparser = parser.add_subparsers(title="OLED screen utilities",
+                                          description="Set of utilities to use pi-top [4]'s OLED screen",
+                                          dest="oled_subcommand")
+
+        parser_draw = subparser.add_parser("draw", help="Draw text and images into the OLED")
+        parser_draw.add_argument("--force", "-f",
+                                 type=int,
+                                 help="Force the hub to give control of the OLED to the Pi",
+                                 default=False,
+                                 )
+        parser_draw.add_argument("--timeout", "-t",
+                                 type=int,
+                                 help="Set the timeout in seconds",
+                                 default=10,
+                                 )
+        parser_draw.add_argument("--font-size", "-s",
+                                 type=int,
+                                 help="Set the font size",
+                                 default=20,
+                                 )
+        parser_draw.add_argument("text",
+                                 help="Set the text to write to screen",
+                                 )
+
+        parser_capture = subparser.add_parser("capture", help="Capture images or videos of the OLED content")
+        parser_capture.add_argument("--force", "-f",
+                                    type=int,
+                                    help="Force the hub to give control of the OLED to the Pi",
+                                    default=False,
+                                    )
+        parser_capture.add_argument("--timeout", "-t",
+                                    type=int,
+                                    help="Set the timeout in seconds",
+                                    default=10,
+                                    )
+        parser_capture.add_argument("--font-size", "-s",
+                                    type=int,
+                                    help="Set the font size",
+                                    default=20,
+                                    )
+        parser_capture.add_argument("text",
+                                    help="Set the text to write to screen",
+                                    )
 
 
 def main():
