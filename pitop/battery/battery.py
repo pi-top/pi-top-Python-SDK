7d90907ff7d24595bbb8f44819dba0811a971808
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2020-12-02 15:13:10 +0000

    Add ptdm subscribe event functionality (#104)
    * Add ptd subscribe event functionality
    
    * Use UUID for button locking; move into /tmp

--- pitop/battery/battery.py
+++ pitop/battery/battery.py
@@ -1,7 +1,63 @@
-from pitopcommon.ptdm import PTDMRequestClient, Message
+from pitopcommon.ptdm import (
+    PTDMRequestClient,
+    PTDMSubscribeClient,
+    Message,
+)
+from pitopcommon.lock import PTLock
+
+import atexit
 
 
 class Battery:
+    def __init__(self):
+        self.when_low = None
+        self.when_critical = None
+        self.when_charging = None
+        self.when_discharging = None
+        self.when_capacity_changed = None
+        self.when_full = None
+
+        self.__ptdm_subscribe_client = None
+        self.__setup_subscribe_client()
+
+        atexit.register(self.__clean_up)
+
+        self.lock = PTLock("pt-battery")
+        self.lock.acquire()
+
+    def __setup_subscribe_client(self):
+        def on_low_battery():
+            self.__ptdm_subscribe_client.invoke_callback_func_if_exists(self.when_low)
+
+        def on_critical_battery():
+            self.__ptdm_subscribe_client.invoke_callback_func_if_exists(self.when_critical)
+
+        def on_state_changed(parameters):
+            charging_state, capacity, time_remaining, wattage = parameters
+
+            if charging_state == 2:
+                self.__ptdm_subscribe_client.invoke_callback_func_if_exists(self.when_full)
+
+            if charging_state == 0:
+                self.__ptdm_subscribe_client.invoke_callback_func_if_exists(self.when_discharging)
+            else:
+                self.__ptdm_subscribe_client.invoke_callback_func_if_exists(self.when_charging)
+
+        self.__ptdm_subscribe_client = PTDMSubscribeClient()
+        self.__ptdm_subscribe_client.initialise({
+            Message.PUB_LOW_BATTERY_WARNING: on_low_battery,
+            Message.PUB_CRITICAL_BATTERY_WARNING: on_critical_battery,
+            Message.PUB_BATTERY_STATE_CHANGED: on_state_changed,
+        })
+        self.__ptdm_subscribe_client.start_listening()
+
+    def __clean_up(self):
+        self.lock.release()
+        try:
+            self.__ptdm_subscribe_client.stop_listening()
+        except Exception:
+            pass
+
     @classmethod
     def get_full_state(cls):
         message = Message.from_parts(Message.REQ_GET_BATTERY_STATE, [])
