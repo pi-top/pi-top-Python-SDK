2248f7f009b8dbff964e5baebd051838ea43bc8e
Ryan Dunwoody <me@ryandunwoody.com>
2020-12-07 22:33:18 +0800

    Update example to match new property usage

--- examples/pma/imu.py
+++ examples/pma/imu.py
@@ -4,9 +4,9 @@ from time import sleep
 imu = Imu()
 
 while True:
-    acc = imu.accelerometer_raw()
-    gyro = imu.gyroscope_raw()
-    mag = imu.magnetometer_raw()
+    acc = imu.accelerometer_raw
+    gyro = imu.gyroscope_raw
+    mag = imu.magnetometer_raw
 
     print("acc: {x}, {y}, {z}".format(**acc))
     print("gyro: {x}, {y}, {z}".format(**gyro))
