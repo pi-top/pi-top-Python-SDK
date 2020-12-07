6a9b621f3f28a5c7025061fee08bcbcb56c88d27
Ryan Dunwoody <me@ryandunwoody.com>
2020-12-07 17:52:22 +0800

    Refactor register definitions, add in raw/orientation data reads

--- pitop/pma/common/imu_registers.py
+++ pitop/pma/common/imu_registers.py
@@ -1,46 +1,52 @@
 from dataclasses import dataclass
+from enum import Enum
 
 
-@dataclass
-class ImuDataRegisters:
-    @dataclass
-    class acceleration:
-        x: int = 0x80
-        y: int = 0x81
-        z: int = 0x82
-
-    @dataclass
-    class gyroscope:
-        x: int = 0x83
-        y: int = 0x84
-        z: int = 0x85
-
-    @dataclass
-    class magnetometer:
-        x: int = 0x86
-        y: int = 0x87
-        z: int = 0x88
-
-    @dataclass
-    class orientation:
-        pitch: int = 0x89
-        roll: int = 0x8A
-        yaw: int = 0x8B
-
-
-class ImuEnableRegisterTypes:
+class ImuRegisterTypes:
     ACC = 0
     GYRO = 1
     MAG = 2
     ORIENTATION = 3
 
 
+class RawDataRegisterTypes:
+    X = 0
+    Y = 1
+    Z = 2
+
+
+class OrientationDataRegisterTypes:
+    ROLL = 0
+    PITCH = 1
+    YAW = 2
+
+
 ImuEnableRegisters = {
-    ImuEnableRegisterTypes.ACC: 0x90,
-    ImuEnableRegisterTypes.GYRO: 0x91,
-    ImuEnableRegisterTypes.MAG: 0x92,
-    ImuEnableRegisterTypes.ORIENTATION: 0x93
+    ImuRegisterTypes.ACC: 0x90,
+    ImuRegisterTypes.GYRO: 0x91,
+    ImuRegisterTypes.MAG: 0x92,
+    ImuRegisterTypes.ORIENTATION: 0x93
 }
 
-
-
+ImuDataRegisters = {
+    ImuRegisterTypes.ACC: {
+        RawDataRegisterTypes.X: 0x80,
+        RawDataRegisterTypes.Y: 0x81,
+        RawDataRegisterTypes.Z: 0x82
+    },
+    ImuRegisterTypes.GYRO: {
+        RawDataRegisterTypes.X: 0x83,
+        RawDataRegisterTypes.Y: 0x84,
+        RawDataRegisterTypes.Z: 0x85
+    },
+    ImuRegisterTypes.MAG: {
+        RawDataRegisterTypes.X: 0x86,
+        RawDataRegisterTypes.Y: 0x87,
+        RawDataRegisterTypes.Z: 0x88
+    },
+    ImuRegisterTypes.ORIENTATION: {
+        OrientationDataRegisterTypes.ROLL: 0x89,
+        OrientationDataRegisterTypes.PITCH: 0x8A,
+        OrientationDataRegisterTypes.YAW: 0x8B
+    }
+}
--- pitop/pma/imu_controller.py
+++ pitop/pma/imu_controller.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from .common.imu_registers import ImuDataRegisters, ImuEnableRegisters, ImuEnableRegisterTypes
+from .common.imu_registers import ImuDataRegisters, ImuEnableRegisters, ImuRegisterTypes, RawDataRegisterTypes, OrientationDataRegisterTypes, ImuRegisters
 from .plate_interface import PlateInterface
 
 
@@ -17,61 +17,60 @@ class ImuController:
         self._mag_enable = False
         self._orientation_enable = False
 
-    def set_imu_config(self, function: ImuEnableRegisterTypes, enable: bool):
+    def set_imu_config(self, function: ImuRegisterTypes, enable: bool):
         self._mcu_device.write_word(self._enable_registers[function], int(enable), little_endian=True, signed=False)
 
     def set_acc_config(self, enable: bool):
-        self.set_imu_config(ImuEnableRegisterTypes.ACC, enable)
+        self.set_imu_config(ImuRegisterTypes.ACC, enable)
         self._acc_enable = enable
 
     def set_gyro_config(self, enable: bool):
-        self.set_imu_config(ImuEnableRegisterTypes.GYRO, enable)
+        self.set_imu_config(ImuRegisterTypes.GYRO, enable)
         self._gyro_enable = enable
 
     def set_mag_config(self, enable: bool):
-        self.set_imu_config(ImuEnableRegisterTypes.MAG, enable)
+        self.set_imu_config(ImuRegisterTypes.MAG, enable)
         self._mag_enable = enable
 
     def set_orientation_config(self, enable: bool):
-        self.set_imu_config(ImuEnableRegisterTypes.ORIENTATION, enable)
+        self.set_imu_config(ImuRegisterTypes.ORIENTATION, enable)
         self._orientation_enable = enable
 
-    def read_imu_data(self, data_type: int):
-        return self._mcu_device.read_signed_word(data_type, little_endian=True)
+    def read_imu_data(self, data_register: int):
+        return self._mcu_device.read_signed_word(data_register, little_endian=True)
+
+    def get_raw_data(self, data_type: int):
+        data = {
+            'x': self.read_imu_data(ImuDataRegisters[data_type][RawDataRegisterTypes.X]),
+            'y': self.read_imu_data(ImuDataRegisters[data_type][RawDataRegisterTypes.Y]),
+            'z': self.read_imu_data(ImuDataRegisters[data_type][RawDataRegisterTypes.Z]),
+        }
+
+        return data
+
+    def get_orientation_data(self):
+        data = {
+            'roll': self.read_imu_data(ImuDataRegisters[ImuRegisterTypes.ORIENTATION][OrientationDataRegisterTypes.ROLL]),
+            'pitch': self.read_imu_data(ImuDataRegisters[ImuRegisterTypes.ORIENTATION][OrientationDataRegisterTypes.PITCH]),
+            'yaw': self.read_imu_data(ImuDataRegisters[ImuRegisterTypes.ORIENTATION][OrientationDataRegisterTypes.YAW])
+        }
+        return data
 
     def get_accelerometer_raw(self):
         if not self._acc_enable:
             self.set_acc_config(enable=True)
 
-        acc_data = {
-            'x': self.read_imu_data(ImuDataRegisters.acceleration.x),
-            'y': self.read_imu_data(ImuDataRegisters.acceleration.y),
-            'z': self.read_imu_data(ImuDataRegisters.acceleration.z),
-        }
-
-        return acc_data
+        return self.get_raw_data(ImuRegisterTypes.ACC)
 
     def get_gyroscope_raw(self):
         if not self._gyro_enable:
             self.set_gyro_config(enable=True)
 
-        gyro_data = {
-            'x': self.read_imu_data(ImuDataRegisters.gyroscope.x),
-            'y': self.read_imu_data(ImuDataRegisters.gyroscope.y),
-            'z': self.read_imu_data(ImuDataRegisters.gyroscope.z),
-        }
-
-        return gyro_data
+        return self.get_raw_data(ImuRegisterTypes.GYRO)
 
     def get_magnetometer_raw(self):
         if not self._mag_enable:
             self.set_mag_config(enable=True)
 
-        mag_data = {
-            'x': self.read_imu_data(ImuDataRegisters.magnetometer.x),
-            'y': self.read_imu_data(ImuDataRegisters.magnetometer.y),
-            'z': self.read_imu_data(ImuDataRegisters.magnetometer.z),
-        }
-
-        return mag_data
+        return self.get_raw_data(ImuRegisterTypes.MAG)
 
