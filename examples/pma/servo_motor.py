49a2d9d6a1850b26da044ba97c02f40f7c8f48eb
Ryan Dunwoody <me@ryandunwoody.com>
2020-12-08 15:20:08 +0800

    Modify example code

--- examples/pma/servo_motor.py
+++ examples/pma/servo_motor.py
@@ -2,29 +2,39 @@ from pitop.pma import ServoMotor
 from time import sleep
 
 servo = ServoMotor("S2")
+TARGET_SPEED = 25
 
 # Scan back and forward across a 180 degree angle range in 10 degree hops
 
-for angle in range(90, -100, -10):
-    sleep(1)
-    print("Setting angle to", angle)
-    servo.set_target_angle(angle, 10)
+for current_angle in range(90, -100, -10):
+    print("Setting angle to", current_angle)
+    servo.set_target_angle(current_angle, TARGET_SPEED)
+    sleep(0.5)
 
-for angle in range(-90, 100, 10):
-    sleep(1)
-    print("Setting angle to", angle)
-    servo.set_target_angle(angle, 10)
+for current_angle in range(-90, 100, 10):
+    print("Setting angle to", current_angle)
+    servo.set_target_angle(current_angle, TARGET_SPEED)
+    sleep(0.5)
 
 sleep(1)
 
-# Scan back and forward by setting the speed and waiting
+# Scan back and forward by setting speed only and reading current angle
+STOP_ANGLE = 80
 
-speed = 10
+print("Setting target speed to ", -TARGET_SPEED)
+servo.target_speed = -TARGET_SPEED
 
-print("Setting target speed to ", -speed)
-servo.target_speed(-speed)
-sleep(5)
+current_angle, current_speed = servo.current_angle_and_speed
 
-print("Setting target speed to ", speed)
-servo.target_speed(speed)
-sleep(5)
+while current_angle > -STOP_ANGLE:
+    current_angle, current_speed = servo.current_angle_and_speed
+    print("current_angle: {} | current_speed: {}".format(current_angle, current_speed))
+    sleep(0.05)
+
+print("Setting target speed to ", TARGET_SPEED)
+servo.target_speed = TARGET_SPEED
+
+while current_angle < STOP_ANGLE:
+    current_angle, current_speed = servo.current_angle_and_speed
+    print("current_angle: {} | current_speed: {}".format(current_angle, current_speed))
+    sleep(0.05)
