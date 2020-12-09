b08d326c192c646fe38730e06d469c4772fdd91e
Ryan Dunwoody <me@ryandunwoody.com>
2020-12-09 12:12:14 +0800

    update example to use new target_angle property

--- examples/pma/servo_motor.py
+++ examples/pma/servo_motor.py
@@ -2,24 +2,23 @@ from pitop.pma import ServoMotor
 from time import sleep
 
 servo = ServoMotor("S2")
-TARGET_SPEED = 25
 
-# Scan back and forward across a 180 degree angle range in 10 degree hops
-
+# Scan back and forward across a 180 degree angle range in 10 degree hops using default servo speed
 for current_angle in range(90, -100, -10):
     print("Setting angle to", current_angle)
-    servo.set_target_angle_and_speed(current_angle, TARGET_SPEED)
+    servo.target_angle = current_angle
     sleep(0.5)
 
 for current_angle in range(-90, 100, 10):
     print("Setting angle to", current_angle)
-    servo.set_target_angle_and_speed(current_angle, TARGET_SPEED)
+    servo.target_angle = current_angle
     sleep(0.5)
 
 sleep(1)
 
 # Scan back and forward by setting speed only and reading current angle
 STOP_ANGLE = 80
+TARGET_SPEED = 25
 
 print("Setting target speed to ", -TARGET_SPEED)
 servo.target_speed = -TARGET_SPEED
