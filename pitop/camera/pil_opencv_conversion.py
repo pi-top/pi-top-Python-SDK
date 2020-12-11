bbca1ab467b74e6b54dac7fbca99f3a3c564eee4
Angus Whitehead <angusjfw@gmail.com>
2020-12-11 12:46:17 +0000

    Improve camera api (#128)
    - Adds blocking `get_frame` method to Camera class
      - This is the simplest and most common API for reading images from a camera. Blocks until a new frame is produced then returns that. Still allows the frame_handler to read the same frames.
      - Existing non-blocking method to get latest frame is renamed to `current_frame`. This has the downside that the same frame may be provided multiple times. 
    ⠀
    - Provides conveniences for conversion between RBG PIL Images to BGR numpy ndarrays for OpenCV
      - I hadn't realised swapping pixel order was required, which makes this a bit much to push on to users.
      - Adds a `pil_to_opencv` and `opencv_to_pil` conversion helpers to pitop.camera helper for conversions (does not require cv2 module)
      - Adds opencv=False boolean parameters to methods that by default give access to a PIL Image, to instead provide the opencv format. These are `get_frame`, `current_frame` and `start_handling_frames`.
    ⠀
    - Adds more camera examples and documentation including of new functionality
      - All examples tested on my pi-top
    ⠀  
    - Improves camera testing
      - Fixes mocking and removes unused mocks
      - Reenables some skipped tests
      - Adds tests for new functionality

---
+++ pitop/camera/pil_opencv_conversion.py
@@ -0,0 +1,31 @@
+import numpy as np
+from PIL import Image
+
+
+# For RGB - BGR conversion we can reverse the third level of the pixels ndarray
+# That is what [:, :, ::-1] does - https://stackoverflow.com/a/14140796
+
+def pil_to_opencv(image):
+    """
+    Converts an RGB Pillow Image into an OpenCV compatible BGR numpy ndarray.
+
+    :type image: PIL.Image.Image
+    :param image: A Pillow Image instance in raw RGB format
+    :rtype: numpy.ndarray
+    :return:
+        A numpy array representing the image in BGR format, as used by default in OpenCV
+    """
+    return np.array(image)[:, :, ::-1]
+
+
+def opencv_to_pil(image):
+    """
+    Converts an OpenCV compatible BGR numpy ndarray into an RGB Pillow Image.
+
+    :type image: numpy.ndarray
+    :param image: Raw BGR image data as a numpy ndarray
+    :rtype: PIL.Image.Image
+    :return:
+        A Pillow Image in RGB format
+    """
+    return Image.fromarray(image[:, :, ::-1])
