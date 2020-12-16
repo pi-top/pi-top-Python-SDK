e970d9930ed202bb9d684e43a74e17e2ba89fe9f
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2020-12-16 17:17:37 +0000

    Add pt-project-files dependency

--- examples/miniscreen/oled/display_an_image.py
+++ examples/miniscreen/oled/display_an_image.py
@@ -1,8 +1,7 @@
-# Example code to draw a static image to the screen
-
 from pitop.miniscreen import OLED
 from time import sleep
 
 oled = OLED()
-oled.draw_image_file("wave.png")
+# Image provided by 'pt-project-files'
+oled.draw_image_file("/usr/share/pt-project-files/images/rocket.gif")
 sleep(2)
