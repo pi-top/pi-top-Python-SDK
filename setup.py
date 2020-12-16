fc47cb3a52c28257cd95952b8febf99c9034f6de
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2020-12-16 20:12:06 +0000

    Add Deprecation Support link to setup.py

--- setup.py
+++ setup.py
@@ -152,6 +152,7 @@ __entry_points__ = {
         # 'pi-top' longhand/easy-to-remember command
         "pi-top=pitopcli.pitop:main",
         # Deprecated console scripts:
+        # (https://github.com/pi-top/pi-top-Python-SDK-Deprecation-Support)
         "pt-battery=pitopcli.battery:main",
         "pt-brightness=pitopcli.display:brightness",
         "pt-devices=pitopcli.devices:main",
