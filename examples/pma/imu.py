f811714728f4d41bb96e95857b021e030dee57a9
duwudi <me@ryandunwoody.com>
2020-12-17 22:03:09 +0800

    Initial IMU implementation (#119)

---
+++ examples/pma/imu.py
@@ -0,0 +1,26 @@
+from pitop.pma import Imu
+from time import sleep
+from dataclasses import fields
+
+imu = Imu()
+
+while True:
+    acc = imu.accelerometer
+    acc_x, acc_y, acc_z = list(getattr(acc, field.name) for field in fields(acc))
+    gyro = imu.gyroscope
+    gyro_x, gyro_y, gyro_z = list(getattr(gyro, field.name) for field in fields(gyro))
+    mag = imu.magnetometer
+    mag_x, mag_y, mag_z = list(getattr(mag, field.name) for field in fields(mag))
+
+    orientation_fusion = imu.orientation
+    roll, pitch, yaw = list(getattr(orientation_fusion, field.name) for field in fields(orientation_fusion))
+
+    orientation_accelerometer = imu.accelerometer_orientation
+    roll_acc, pitch_acc, _ = list(getattr(orientation_accelerometer, field.name) for field in fields(orientation_accelerometer))
+
+    print("acc: {}, {}, {}".format(acc_x, acc_y, acc_z))
+    print("gyro: {}, {}, {}".format(gyro_x, gyro_y, gyro_z))
+    print("mag: {}, {}, {}".format(mag_x, mag_y, mag_z))
+    print("orientation_fusion: {}, {}, {}".format(roll, pitch, yaw))
+    print("orientation_accelerometer: {}, {}".format(roll_acc, pitch_acc))
+    sleep(0.1)
--- pitop/pma/__init__.py
+++ pitop/pma/__init__.py
@@ -11,6 +11,7 @@ from .ultrasonic_sensor import UltrasonicSensor  # noqa: F401
 from .joystick import Joystick  # noqa: F401
 from .encoder_motor import EncoderMotor  # noqa: F401
 from .servo_motor import ServoMotor  # noqa: F401
+from .imu import Imu  # noqa: F401
 
 from .parameters import (  # noqa: F401
     ForwardDirection,
