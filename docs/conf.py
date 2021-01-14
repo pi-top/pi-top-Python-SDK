1fe7e5c664bebd40e4e329708bc0b9aaf51a2462
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2021-01-14 16:10:12 +0000

    Try mocking common lib singleton

--- docs/conf.py
+++ docs/conf.py
@@ -115,6 +115,7 @@ autodoc_mock_imports = [
     'pitopcommon.lock',
     'pitopcommon.logger',
     'pitopcommon.ptdm',
+    'pitopcommon.singleton',
     'pitopcommon.smbus_device',
     'pitopcommon.sys_config',
     'PIL',
