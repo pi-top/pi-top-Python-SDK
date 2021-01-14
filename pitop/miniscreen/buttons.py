351666b5c42324515c7b059f092b1b2ca7df995f
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2021-01-14 12:43:44 +0000

    Move miniscreen components up a directory

--- pitop/miniscreen/buttons.py
+++ pitop/miniscreen/buttons.py
@@ -28,10 +28,9 @@ class Singleton(type):
     def __call__(cls, *args, **kwargs):
         if cls.instance is None:
             print("Creating NEW Singleton instance")
+            cls.instance = super(Singleton, cls).__call__(*args, **kwargs)
         else:
             print("Using EXISTING Singleton instance")
-
-            cls.instance = super(Singleton, cls).__call__(*args, **kwargs)
 
         return cls.instance
 
