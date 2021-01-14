6902f9befeef22939b9d2e67842fd7dcfe3f3229
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2021-01-14 16:28:57 +0000

    Battery: use lambda funcs (#158)

--- pitop/battery/battery.py
+++ pitop/battery/battery.py
@@ -1,3 +1,4 @@
+from pitopcommon.logger import PTLogger
 from pitopcommon.ptdm import (
     PTDMRequestClient,
     PTDMSubscribeClient,
@@ -16,33 +17,42 @@ class Battery:
         # self.when_capacity_changed = None
         self.when_full = None
 
+        self.__previous_charging_state = Battery.get_full_state()[0]
+
         self.__ptdm_subscribe_client = None
         self.__setup_subscribe_client()
 
         atexit.register(self.__clean_up)
 
     def __setup_subscribe_client(self):
-        def on_low_battery():
-            self.__ptdm_subscribe_client.invoke_callback_func_if_exists(self.when_low)
-
-        def on_critical_battery():
-            self.__ptdm_subscribe_client.invoke_callback_func_if_exists(self.when_critical)
-
         def on_state_changed(parameters):
-            charging_state, capacity, time_remaining, wattage = parameters
+            charging_state = int(parameters[0])
 
-            if charging_state == 2:
-                self.__ptdm_subscribe_client.invoke_callback_func_if_exists(self.when_full)
+            if charging_state not in range(0, 3):
+                PTLogger.warning("Invalid charging state from pi-top device manager")
+                return
 
-            if charging_state == 0:
-                self.__ptdm_subscribe_client.invoke_callback_func_if_exists(self.when_discharging)
-            else:
-                self.__ptdm_subscribe_client.invoke_callback_func_if_exists(self.when_charging)
+            if charging_state == self.__previous_charging_state:
+                PTLogger.debug("Charging state has not changed - doing nothing...")
+                return
+
+            self.__previous_charging_state = charging_state
+
+            funcs_to_invoke = {
+                2: self.when_full,
+                1: self.when_charging,
+                0: self.when_discharging,
+            }
+
+            func = funcs_to_invoke[charging_state]
+
+            if callable(func):
+                func()
 
         self.__ptdm_subscribe_client = PTDMSubscribeClient()
         self.__ptdm_subscribe_client.initialise({
-            Message.PUB_LOW_BATTERY_WARNING: on_low_battery,
-            Message.PUB_CRITICAL_BATTERY_WARNING: on_critical_battery,
+            Message.PUB_LOW_BATTERY_WARNING: lambda: self.when_low,
+            Message.PUB_CRITICAL_BATTERY_WARNING: lambda: self.when_critical,
             Message.PUB_BATTERY_STATE_CHANGED: on_state_changed,
         })
         self.__ptdm_subscribe_client.start_listening()
