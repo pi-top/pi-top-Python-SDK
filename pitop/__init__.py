28fb9890e555fdbf123521b0bfd3d60d4bfe2e4f
Jorge Capona <jcapona@gmail.com>
2021-01-19 11:11:03 -0300

    Add ImageProcessor class

--- pitop/__init__.py
+++ pitop/__init__.py
@@ -1,2 +1,3 @@
 from .system.pitop import PiTop
 from .robotics.rover_alex import RoverAlex
+from .processing.image_processor import ImageProcessor
---
+++ pitop/processing/image_processor.py
@@ -0,0 +1,16 @@
+class ImageProcessor:
+
+    def __init__(self, frame_source):
+        self.on_new_frame = None
+        self.__process = frame_source
+        self.transforms = list()
+
+    def add_transform(self, transform_function):
+        self.transforms.append(transform_function)
+
+    def __process(self, frame):
+        for transform_fcn in self.transforms:
+            frame, result = transform_fcn(frame)
+
+        if callable(self.on_new_frame):
+            self.on_new_frame((frame, result))
