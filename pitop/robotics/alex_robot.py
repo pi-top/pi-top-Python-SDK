fc1e4546dd560be1b270d65d6691167b107da185
Jorge Capona <jcapona@gmail.com>
2021-01-23 16:59:42 -0300

    Add AlexRobot calibration routine

--- pitop/robotics/alex_robot.py
+++ pitop/robotics/alex_robot.py
@@ -9,8 +9,13 @@ from pitopcommon.common_ids import FirmwareDeviceID
 from .drive_controller import DriveController
 from .pan_tilt_controller import PanTiltController
 
+import configparser
+from os.path import exists
+
 
 class AlexRobot(PiTop):
+    CONF_FILE = "/etc/pi-top/sdk/alex.conf"
+
     def __init__(self,
                  motor_left_port="M3",
                  motor_right_port="M0",
@@ -31,7 +36,7 @@ class AlexRobot(PiTop):
         self.left_motor = self.port_manager.get_component(motor_left_port)
         self.right_motor = self.port_manager.get_component(motor_right_port)
 
-        self._pan_tilt_controller = PanTiltController()
+        self._pan_tilt_controller = PanTiltController(servo_pan_port=servo_pan_port, servo_tilt_port=servo_tilt_port)
         self.pan_servo = self.port_manager.get_component(servo_pan_port)
         self.tilt_servo = self.port_manager.get_component(servo_tilt_port)
 
@@ -55,3 +60,68 @@ class AlexRobot(PiTop):
 
     def store_servo_zero_points(self):
         pass
+
+    def calibrate(self, save=True, reset=False):
+        if not reset and exists(self.CONF_FILE):
+            return self.__load_calibration()
+
+        # PanTilt servo calibration
+        servo_lookup = {
+            'pan_zero_point': self.pan_servo,
+            'tilt_zero_point': self.tilt_servo,
+        }
+        for servo_name, servo_obj in servo_lookup.items():
+            value = self.__calibrate_servo(servo_name, servo_obj)
+            if save and value is not None:
+                self.__save_calibration(section='PAN_TILT', values_dict={servo_name: value})
+
+    def __calibrate_servo(self, servo_name, servo_obj):
+        print(f"Starting {servo_name} servo motor calibration.")
+
+        while True:
+            servo_obj.target_angle = 0
+            print("Enter a angle to use as zero point.")
+            user_zero_setting = input("Value: ")
+            try:
+                user_zero_setting = int(user_zero_setting)
+                if user_zero_setting < int(servo_obj.angle_range[0]) or user_zero_setting > int(servo_obj.angle_range[1]):
+                    print(f"Invalid angle value {user_zero_setting}. "
+                          " Please enter a value in the range {servo_obj.angle_range}")
+                    continue
+            except ValueError:
+                print("Please, enter a valid value")
+                continue
+
+            servo_obj.target_angle = user_zero_setting
+
+            print("Is the servo horn aligned with the body?")
+            finished_calibration = str(input("'y' to accept, 'n' to try again, 'e' to exit: "))
+            if finished_calibration.upper() == 'Y':
+                servo_obj.zero_point = user_zero_setting
+                return user_zero_setting
+            elif finished_calibration.upper() == 'E':
+                return None
+
+    def __load_calibration(self):
+        config = configparser.ConfigParser()
+        config.read(self.CONF_FILE)
+
+        if 'PAN_TILT' in config.sections():
+            section_config = config['PAN_TILT']
+            if section_config.get('pan_zero_point'):
+                print(f"PanTilt.pan_servo.zero = {int(section_config.get('pan_zero_point'))}")
+                self.pan_servo.zero_point = int(section_config.get('pan_zero_point'))
+
+            if section_config.get('tilt_zero_point'):
+                print(f"PanTilt.tilt_servo.zero = {int(section_config.get('tilt_zero_point'))}")
+                self.tilt_servo.zero_point = int(section_config.get('tilt_zero_point'))
+
+    def __save_calibration(self, section, values_dict):
+        config = configparser.ConfigParser()
+        config.read(self.CONF_FILE)
+        if section not in config:
+            config[section] = {}
+
+        config[section].update({k: str(v) for k, v in values_dict.items()})
+        with open(self.CONF_FILE, 'w') as configfile:
+            config.write(configfile)
--- pitop/robotics/pan_tilt_controller.py
+++ pitop/robotics/pan_tilt_controller.py
@@ -4,14 +4,9 @@ from pitop.system.port_manager import PortManager
 
 class PanTiltController:
 
-    def __init__(self, pan_servo_port="S0", tilt_servo_port="S3"):
-        # TODO: get zero points from file, defaults should be zero
-        self._pan_servo = ServoMotor(pan_servo_port)
-        self._tilt_servo = ServoMotor(tilt_servo_port)
+    def __init__(self, servo_pan_port="S0", servo_tilt_port="S3"):
+        self._pan_servo = ServoMotor(servo_pan_port)
+        self._tilt_servo = ServoMotor(servo_tilt_port)
         self.__port_manager = PortManager()
-        self.__port_manager.register_component_instance(self._pan_servo, pan_servo_port)
-        self.__port_manager.register_component_instance(self._tilt_servo, tilt_servo_port)
-
-    def store_zero_points(self):
-        # TODO: save to file
-        pass
+        self.__port_manager.register_component_instance(self._pan_servo, servo_pan_port)
+        self.__port_manager.register_component_instance(self._tilt_servo, servo_tilt_port)
