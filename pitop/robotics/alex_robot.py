7e3da3306babaca453006c58c36fc31b2b2dc80d
Ryan Dunwoody <me@ryandunwoody.com>
2021-01-23 16:26:48 +0800

    Change sensor port defaults and rename servos to pan and tilt

--- pitop/robotics/alex_robot.py
+++ pitop/robotics/alex_robot.py
@@ -12,12 +12,12 @@ from .drive_controller import DriveController
 
 class AlexRobot(PiTop):
     def __init__(self,
-                 motor_left_port="M1",
-                 motor_right_port="M2",
+                 motor_left_port="M3",
+                 motor_right_port="M0",
                  camera_port=0,
-                 ultrasonic_sensor_port="D1",
-                 servo_left_port="S1",
-                 servo_right_port="S2"
+                 ultrasonic_sensor_port="D3",
+                 servo_pan_port="S0",
+                 servo_tilt_port="S3"                 
                  ):
 
         super().__init__()
@@ -26,8 +26,8 @@ class AlexRobot(PiTop):
 
         self.camera = self.port_manager.register_component(Camera, camera_port)
         self.ultrasonic_sensor = self.port_manager.register_component(UltrasonicSensor, ultrasonic_sensor_port)
-        self.servo_left = self.port_manager.register_component(ServoMotor, servo_left_port)
-        self.servo_right = self.port_manager.register_component(ServoMotor, servo_right_port)
+        self.servo_left = self.port_manager.register_component(ServoMotor, servo_tilt_port)
+        self.servo_right = self.port_manager.register_component(ServoMotor, servo_pan_port)
 
         self._drive_controller = DriveController(motor_left_port, motor_right_port)
         self.motor_left = self.port_manager.get_component(motor_left_port)
