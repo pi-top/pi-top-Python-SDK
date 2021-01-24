899608dd6919b3aa1e7f6b8e43656e9a7d2bc51b
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2021-01-24 13:34:44 +0000

    Remove unnecessary guide in commented text

--- pitop/robotics/pid_controller.py
+++ pitop/robotics/pid_controller.py
@@ -1,22 +1,6 @@
 from simple_pid import PID
 
 
-# // short guide for tuning PIDs \\ #
-# a) Set integral and derivative gains to zero (work only with the P part).
-#
-# b) Vary this proportional gain until you obtain the best possible performance.
-#
-# c) Keeping this prop. gain, now vary the integral gain. This fact will reduce
-# the steady state error but (as expected) it may introduce some oscillations and overshoot. Select the best one like
-# this.
-#
-# d) You should have obtained a fast response system but with (possibly) high overshot and oscillations. Now,
-# keeping fixed the above mentioned gains, vary the derivative gain. This will damp your system reducing oscillations
-# and overshoot. Naturally, this new variation may affect the general performance but try to select the best deriv.
-# gain.
-#
-# e) Return to step (a) in order to improve your closed loop performance as best as possible.
-
 # TODO: decide whether to remove this and use simple_pid directly in drive_controller
 class PIDController:
     def __init__(self, lower_limit, upper_limit, setpoint, Kp, Ki, Kd):
