10ed08a8335bdb9ee2f733dff5968ad6ba9e363a
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2021-02-01 18:33:46 +0000

    Add all relevant components top level

--- pitop/__init__.py
+++ pitop/__init__.py
@@ -1,2 +1,31 @@
+# Top-level
 from .system.pitop import PiTop
+
+# PMA
+from .pma.button import Button
+from .pma.buzzer import Buzzer
+from .pma.led import LED
+from .pma.light_sensor import LightSensor
+from .pma.potentiometer import Potentiometer
+from .pma.sound_sensor import SoundSensor
+from .pma.ultrasonic_sensor import UltrasonicSensor
+
+from .pma.imu import IMU
+from .pma.encoder_motor import EncoderMotor
+from .pma.servo_motor import (
+    ServoMotor,
+    ServoMotorState
+)
+
+from .pma.parameters import (
+    ForwardDirection,
+    Direction,
+    BrakingType
+)
+
+# Robotics
 from .robotics.alex_robot import AlexRobot
+
+# System Devices
+from .camera import Camera
+from .keyboard import KeyboardButton
