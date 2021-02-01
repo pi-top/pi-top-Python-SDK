f5c63f5a9a891b252054d8c9dd703ea359d0d2b2
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2021-02-01 17:27:19 +0000

    Fix example

--- examples/camera/camera_image_processing_stream_opencv.py
+++ examples/camera/camera_image_processing_stream_opencv.py
@@ -1,21 +1,24 @@
 from pitop.camera import Camera
 import cv2
+from time import sleep
 
 cam = Camera(format='OpenCV')
 
 
-def show_image(image):
-    print("Showing image")
-    print(image.shape)
+def show_gray_image(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     cv2.imshow('frame', gray)
+    cv2.waitKey(1)  # Necessary to show image
 
 
+# Use callback function
+cam.on_frame = show_gray_image
+sleep(60)
+
+
+# Use get_frame
 while True:
-    show_image(
-        cam.get_frame()
-    )
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    show_gray_image(cam.get_frame())
+
 
 cv2.destroyAllWindows()
