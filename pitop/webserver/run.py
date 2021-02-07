d31e23b819b1610c1e2e317068759eeaadb0f8e7
Ryan Dunwoody <me@ryandunwoody.com>
2021-02-07 18:40:44 +0800

    run calibration

--- pitop/webserver/run.py
+++ pitop/webserver/run.py
@@ -85,6 +85,7 @@ if __name__ == "__main__":
     from geventwebsocket.handler import WebSocketHandler
 
     alex = AlexRobot()
+    alex.calibrate()
     alex.pan_servo.target_angle = 0
     alex.tilt_servo.target_angle = 0
     frame_bytes = None
