fc97c582d513b73515d98fdd65a52d1b7f2fdfa1
Jorge Capona <jcapona@gmail.com>
2021-03-08 10:01:20 -0300

    Remove unused SupportsCamera mixin

--- pitop/core/mixins/__init__.py
+++ pitop/core/mixins/__init__.py
@@ -2,7 +2,6 @@ from .componentable import Componentable
 from .recreatable import Recreatable
 from .stateful import Stateful
 from .supports_battery import SupportsBattery
-from .supports_camera import SupportsCamera
 from .supports_driving import SupportsDriving
 from .supports_miniscreen import SupportsMiniscreen
 from .supports_pan_tilt import SupportsPanTilt
