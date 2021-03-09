15d813a1b6fd58fcc841f0906d1392d7a0d94496
Ryan Dunwoody <me@ryandunwoody.com>
2021-03-09 20:18:48 +0800

    Simplify example using SDK class

--- examples/camera/aruco_marker_detect.py
+++ examples/camera/aruco_marker_detect.py
@@ -1,58 +1,23 @@
-from pitop.camera import Camera, load_camera_cal
+from pitop.camera import Camera
+from pitop.processing.algorithms import ArucoMarkers
 from signal import pause
 import cv2
-import sys
-
-
-def draw_axis(frame, rotation_vectors, translation_vectors):
-    for marker_rvec, marker_tvec in zip(rotation_vectors, translation_vectors):
-        cv2.aruco.drawAxis(frame, mtx, dist, marker_rvec, marker_tvec, 0.05)
-
-
-def draw_markers(frame, ids, corners):
-    cv2.aruco.drawDetectedMarkers(frame, corners)
-    for marker_id, marker_corners in zip(ids, corners):
-        top_left_corner = marker_corners[0][0]
-        cv2.putText(frame, str(marker_id),
-                    (int(top_left_corner[0]), int(top_left_corner[1] - 15)),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (162, 178, 0), 2)
-
-
-def get_marker_centers(corners):
-    marker_centers = []
-    for marker_corner in corners:
-        corners = marker_corner.reshape((4, 2))
-        top_left, top_right, bottom_right, bottom_left = corners
-        c_x = int((top_left[0] + bottom_right[0]) / 2.0)
-        c_y = int((top_left[1] + bottom_right[1]) / 2.0)
-        marker_centers.append([c_x, c_y])
-
-    return marker_centers
 
 
 def aruco_detect(frame):
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    corners, ids, rejected = cv2.aruco.detectMarkers(gray_frame, aruco_dict, parameters=aurco_params)
-    annotated_image = frame
-
-    if len(corners) > 0:
-        rotation_vectors, translation_vectors = cv2.aruco.estimatePoseSingleMarkers(corners, 0.05, mtx, dist)
-        marker_centers = get_marker_centers(corners)
-        draw_markers(annotated_image, ids, corners)
-        draw_axis(annotated_image, rotation_vectors, translation_vectors)
-
-    cv2.imshow("Image", annotated_image)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        sys.exit()
+    if aruco_markers.detect(frame):
+        aruco_markers.draw_markers(frame)
+        aruco_markers.draw_axis(frame)
+        ids = aruco_markers.ids
+        centers = aruco_markers.centers
+        for id, center in zip(ids, centers):
+            print(f'ID: {id} | Center: {center}')
+    cv2.imshow("Image", frame)
+    cv2.waitKey(1)
 
 
 camera = Camera(0, (640, 480), rotate_angle=0, format='opencv')
-aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
-aurco_params = cv2.aruco.DetectorParameters_create()
-mtx, dist = load_camera_cal()
-
-
+aruco_markers = ArucoMarkers()
 camera.on_frame = aruco_detect
-
 
 pause()
