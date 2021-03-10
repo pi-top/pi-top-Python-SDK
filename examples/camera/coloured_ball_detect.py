b66b2c4cf373e8538b2ad096dde80a2946056f40
Ryan Dunwoody <me@ryandunwoody.com>
2021-03-10 19:25:27 +0800

    Add coloured ball detect example

---
+++ examples/camera/coloured_ball_detect.py
@@ -0,0 +1,19 @@
+from pitop.camera import Camera
+from pitop.processing.algorithms import ball_detect
+from signal import pause
+import cv2
+
+
+def process_frame_for_ball(frame):
+    ball_data = ball_detect(frame, colour='red', image_format="OpenCV")
+    if ball_data.center is not None:
+        center = ball_data.center
+        print(f'Center: {center} | Radius: {ball_data.radius}')
+    cv2.imshow("Image", ball_data.robot_view)
+    cv2.waitKey(1)
+
+
+camera = Camera(0, (640, 480), rotate_angle=90)
+camera.on_frame = process_frame_for_ball
+
+pause()
