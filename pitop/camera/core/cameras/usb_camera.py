dccf658998d00f89374a1edaf4ba131ebecf4a0f
Jorge Capona <jcapona@gmail.com>
2021-03-16 16:40:07 -0300

    Loop over available devices to create camera object

--- pitop/camera/core/cameras/usb_camera.py
+++ pitop/camera/core/cameras/usb_camera.py
@@ -1,22 +1,34 @@
 from PIL import Image
+from os import listdir, system
 
 from PyV4L2Camera.camera import Camera as V4L2Camera
 from PyV4L2Camera.exceptions import CameraError as V4L2CameraError
 
 
 class UsbCamera:
-    def __init__(self, index: int = 0, resolution=None):
-        self.index = index
+    def __init__(self, index=None, resolution=None):
+        # if no index is provided, loop over available video devices
+        indexes = self.list_device_indexes() if index is None else [index]
         self.__camera = None
+        self.index = None
 
-        try:
-            if resolution is not None:
-                self.__camera = V4L2Camera(f"/dev/video{index}", resolution[0], resolution[1])
-            else:
-                self.__camera = V4L2Camera(f"/dev/video{index}")
+        for idx in indexes:
+            try:
+                self.__camera = self.create_camera_object(idx, resolution)
+                if self.__camera:
+                    self.index = idx
+                    break
+            except V4L2CameraError:
+                continue
 
-        except V4L2CameraError:
-            raise IOError(f"Error opening camera {index}. Make sure it's correctly connected via USB.") from None
+        if self.__camera is None:
+            raise IOError("Error opening camera. Make sure it's correctly connected via USB.") from None
+
+    def create_camera_object(self, index, resolution=None):
+        if resolution is not None:
+            return V4L2Camera(f"/dev/video{index}", resolution[0], resolution[1])
+        else:
+            return V4L2Camera(f"/dev/video{index}")
 
     def __del__(self):
         if hasattr(self.__camera, "close"):
@@ -31,3 +43,25 @@ class UsbCamera:
             'raw',
             'RGB'
         )
+
+    @staticmethod
+    def list_device_indexes():
+        indexes = []
+        device_names = [name for name in listdir("/dev") if "video" in name]
+
+        # Not all devices actually provide video
+        # eg: video1 can be a metadata device for video0
+        for device in device_names:
+            cmd = f"v4l2-ctl --list-formats --device /dev/{device} | grep -qE '[[0-9]]'"
+            try:
+                if system(cmd) != 0:
+                    continue
+                index = int(device[len("video"):])
+                # indexes > 10 are for bcm2835, not useful for us
+                if index < 10:
+                    indexes.append(index)
+            except Exception:
+                continue
+
+        indexes.sort()
+        return indexes
