3103ed26604ce0fa65cc8011604a8bf152f6f382
Ryan Dunwoody <me@ryandunwoody.com>
2021-03-17 10:51:10 +0800

    Modify pincer controller to match drive controller and new servo setting method

--- pitop/robotics/pincer_controller.py
+++ pitop/robotics/pincer_controller.py
@@ -1,30 +1,48 @@
-from pitop.pma import ServoMotor, ServoMotorState
-from pitop.system.port_manager import PortManager
+from pitop.pma import ServoMotor, ServoMotorSetting
+from pitop.core.exceptions import UninitializedComponent
+from pitop.core.mixins import (
+    Stateful,
+    Recreatable,
+)
 
 
-class PincerController:
+class PincerController(Stateful, Recreatable):
+    """Represents a pincer that uses two servo motors connected parallel to each other."""
+    _initialized = False
 
-    def __init__(self, right_pincer_port="S0", left_pincer_port="S3"):
+    def __init__(self, right_pincer_port="S0", left_pincer_port="S3", name="pincers"):
+        self.name = name
         self._right_pincer = ServoMotor(right_pincer_port)
         self._left_pincer = ServoMotor(left_pincer_port)
-        self.__right_pincer_state = ServoMotorState()
-        self.__left_pincer_state = ServoMotorState()
-        self.__port_manager = PortManager()
-        self.__port_manager.register_pma_component(self._right_pincer)
-        self.__port_manager.register_pma_component(self._left_pincer)
+        self.__right_pincer_setting = ServoMotorSetting()
+        self.__left_pincer_setting = ServoMotorSetting()
 
+        Stateful.__init__(self, children=['left_pincer', 'right_pincer'])
+        Recreatable.__init__(self,
+                             config_dict={"left_pincer_port": left_pincer_port, "right_pincer_port": right_pincer_port,
+                                          "name": self.name})
+
+    def is_initialized(fcn):
+        def check_initialization(self, *args, **kwargs):
+            if not self._initialized:
+                raise UninitializedComponent("PincerController not initialized")
+            return fcn(self, *args, **kwargs)
+        return check_initialization
+
+    @is_initialized
     def close(self, speed: int = 100, angle: int = 0):
-        self.__right_pincer_state.speed = speed
-        self.__right_pincer_state.angle = angle
-        self.__left_pincer_state.speed = speed
-        self.__left_pincer_state.angle = angle
-        self._right_pincer.state = self.__right_pincer_state
-        self._left_pincer.state = self.__left_pincer_state
+        self.__right_pincer_setting.speed = speed
+        self.__right_pincer_setting.angle = angle
+        self.__left_pincer_setting.speed = speed
+        self.__left_pincer_setting.angle = angle
+        self._right_pincer.setting = self.__right_pincer_setting
+        self._left_pincer.setting = self.__left_pincer_setting
 
+    @is_initialized
     def open(self, speed: int = 50, angle: int = 45):
-        self.__right_pincer_state.speed = speed
-        self.__right_pincer_state.angle = angle
-        self.__left_pincer_state.speed = speed
-        self.__left_pincer_state.angle = -angle
-        self._right_pincer.state = self.__right_pincer_state
-        self._left_pincer.state = self.__left_pincer_state
+        self.__right_pincer_setting.speed = speed
+        self.__right_pincer_setting.angle = angle
+        self.__left_pincer_setting.speed = speed
+        self.__left_pincer_setting.angle = -angle
+        self._right_pincer.setting = self.__right_pincer_setting
+        self._left_pincer.setting = self.__left_pincer_setting
