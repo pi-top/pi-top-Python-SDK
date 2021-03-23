1329d61e62d6b58c1da641d1b7541b1a371cb4f6
Jorge Capona <jcapona@users.noreply.github.com>
2021-03-23 06:35:39 -0700

    ServoMotor: don't use setting on angle/speed properties (#272)

--- examples/pma/servo_motor.py
+++ examples/pma/servo_motor.py
@@ -1,19 +1,19 @@
-from pitop import ServoMotor, ServoMotorState
+from pitop import ServoMotor, ServoMotorSetting
 from time import sleep
 
 servo = ServoMotor("S0")
 
-# Scan back and forward across a 180 degree angle range in 10 degree hops using default servo speed
-for angle in range(90, -100, -10):
+# Scan back and forward across a 180 degree angle range in 30 degree hops using default servo speed
+for angle in range(90, -100, -30):
     print("Setting angle to", angle)
     servo.target_angle = angle
     sleep(0.5)
 
 # you can also set angle with a different speed than the default
-servo_settings = ServoMotorState()
+servo_settings = ServoMotorSetting()
 servo_settings.speed = 25
 
-for angle in range(-90, 100, 10):
+for angle in range(-90, 100, 30):
     print("Setting angle to", angle)
     servo_settings.angle = angle
     servo.setting = servo_settings
@@ -21,16 +21,18 @@ for angle in range(-90, 100, 10):
 
 sleep(1)
 
-# Scan back and forward by setting speed only and reading current angle
+# Scan back and forward displaying current angle and speed
 STOP_ANGLE = 80
-TARGET_SPEED = 25
+TARGET_SPEED = 40
 
-print("Setting target speed to ", -TARGET_SPEED)
+print("Sweeping using speed ", -TARGET_SPEED)
 servo.target_speed = -TARGET_SPEED
 
 current_state = servo.setting
 current_angle = current_state.angle
 
+# sweep using the already set servo speed
+servo.sweep()
 while current_angle > -STOP_ANGLE:
     current_state = servo.setting
     current_angle = current_state.angle
@@ -38,9 +40,10 @@ while current_angle > -STOP_ANGLE:
     print(f"current_angle: {current_angle} | current_speed: {current_speed}")
     sleep(0.05)
 
-print("Setting target speed to ", TARGET_SPEED)
-servo.target_speed = TARGET_SPEED
+print("Sweeping using speed ", TARGET_SPEED)
 
+# you can also sweep specifying the speed when calling the sweep method
+servo.sweep(speed=TARGET_SPEED)
 while current_angle < STOP_ANGLE:
     current_state = servo.setting
     current_angle = current_state.angle
