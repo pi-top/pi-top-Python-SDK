738148924729bfe0400ee7de32ee0ce87ff5c642
Ryan Dunwoody <me@ryandunwoody.com>
2021-03-23 15:25:00 +0800

    Change draw colour based on ball colour

--- pitop/processing/algorithms/ball_detect.py
+++ pitop/processing/algorithms/ball_detect.py
@@ -5,7 +5,6 @@ import imutils
 from .line_detect import get_control_angle, centroid_reposition
 from pitop.core import ImageFunctions
 from pitop.processing.utils.vision_functions import (
-    find_centroid,
     scale_frame
 )
 
@@ -17,7 +16,7 @@ class DotDict(dict):
     __delattr__ = dict.__delitem__
 
 
-colours = {
+colour_ranges = {
     'red': {
         'lower': (150, 75, 75),
         'upper': (200, 255, 255)
@@ -32,7 +31,13 @@ colours = {
     }
 }
 
-BUFFER_LENGTH = 64
+draw_colour = {
+    'red': (0, 0, 255),
+    'green': (0, 255, 0),
+    'blue': (255, 0, 0)
+}
+
+BUFFER_LENGTH = 32
 detection_points = deque(maxlen=BUFFER_LENGTH)
 
 
@@ -43,8 +48,8 @@ def colour_filter(frame, colour: str = "red", return_binary_mask=False, image_fo
     if colour.lower() not in ('red', 'green', 'blue'):
         raise ValueError('colour must be "red", "green" or "blue"')
 
-    hsv_lower = colours[colour]['lower']
-    hsv_upper = colours[colour]['upper']
+    hsv_lower = colour_ranges[colour]['lower']
+    hsv_upper = colour_ranges[colour]['upper']
     blurred = cv2.GaussianBlur(frame, (11, 11), 0)
     hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, hsv_lower, hsv_upper)
@@ -60,11 +65,11 @@ def colour_filter(frame, colour: str = "red", return_binary_mask=False, image_fo
     return mask
 
 
-def ball_detect(frame, colour: str = "red", image_return_format: str ="PIL", scale_factor=0.5):
+def process_frame_for_ball(frame, colour: str = "red", image_return_format: str = "PIL", scale_factor=0.5):
     cv_frame = ImageFunctions.convert(frame, format="OpenCV")
     resized_frame = scale_frame(cv_frame, scale=scale_factor)
 
-    mask = colour_filter(cv_frame, colour=colour, return_binary_mask=True, image_format="OpenCV",
+    mask = colour_filter(resized_frame, colour=colour, return_binary_mask=True, image_format="OpenCV",
                          image_return_format="OpenCV")
 
     contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -81,14 +86,16 @@ def ball_detect(frame, colour: str = "red", image_return_format: str ="PIL", sca
         moment_matrix = cv2.moments(c)
         center = (int(moment_matrix["m10"] / moment_matrix["m00"]), int(moment_matrix["m01"] / moment_matrix["m00"]))
         # only proceed if the radius meets a minimum size
-        if radius / resized_frame.size[0] > 5:
+        if radius > 5:
             # draw the circle and centroid on the frame,
             # then update the list of tracked points
             cv2.circle(resized_frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
-            cv2.circle(resized_frame, center, 5, (0, 0, 255), -1)
+            cv2.circle(resized_frame, center, 5, draw_colour[colour], -1)
             detection_points.appendleft(center)
-        # reposition centre to (0, 0) is in the middle. Keep scale as 1 as user sees scaled down 320x240 image
-        center = centroid_reposition(center, 1, resized_frame)
+            # reposition centre to (0, 0) is in the middle. Keep scale as 1 as user sees scaled down 320x240 image
+            center = centroid_reposition(center, 1, resized_frame)
+        else:
+            center = None
 
     for i in range(1, len(detection_points)):
         # if either of the tracked points are None, ignore
@@ -97,11 +104,12 @@ def ball_detect(frame, colour: str = "red", image_return_format: str ="PIL", sca
             continue
         # otherwise, compute the thickness of the line and
         # draw the connecting lines
-        thickness = int(np.sqrt(BUFFER_LENGTH / float(i + 1)) * 2.5)
-        cv2.line(resized_frame, detection_points[i - 1], detection_points[i], (0, 0, 255), thickness)
+        thickness = int(np.sqrt(BUFFER_LENGTH / float(i + 1)))
+
+        cv2.line(resized_frame, detection_points[i - 1], detection_points[i], draw_colour[colour], thickness)
 
     if image_return_format.lower() != 'opencv':
-        robot_view = ImageFunctions.convert(cv_frame, format="PIL")
+        robot_view = ImageFunctions.convert(resized_frame, format="PIL")
     else:
         robot_view = resized_frame
 
