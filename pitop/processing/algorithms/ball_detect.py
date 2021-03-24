588970cacb381cbfca497fd245124294658d7686
Ryan Dunwoody <me@ryandunwoody.com>
2021-03-24 20:44:43 +0800

    Add found ball boolean value
    makes it easier for users to understand the code

--- pitop/processing/algorithms/ball_detect.py
+++ pitop/processing/algorithms/ball_detect.py
@@ -126,6 +126,7 @@ def process_frame_for_ball(frame, colours=("red",), image_return_format: str = "
     ball_centers = {}
     ball_radii = {}
     ball_angles = {}
+    ball_finds = {}
     for colour in colours:
         mask = colour_filter(resized_frame, colour=colour, return_binary_mask=True, image_format="OpenCV",
                              image_return_format="OpenCV")
@@ -133,19 +134,29 @@ def process_frame_for_ball(frame, colours=("red",), image_return_format: str = "
         contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         contours = imutils.grab_contours(contours)  # fixes problems with OpenCV changing their protocol
 
-        ball_center, ball_radius = find_most_likely_ball(contours, colours, resized_frame)
+        ball_center, ball_radius = find_most_likely_ball(contours, colour, resized_frame)
 
         detection_points[colour].appendleft(ball_center)
 
         if ball_center is not None:
             # draw the circle and centroid on the frame, then update the list of tracked points
             cv2.circle(resized_frame, ball_center, ball_radius, (0, 255, 255), 2)
-            cv2.circle(resized_frame, ball_center, 5, draw_colour[colours], -1)
+            cv2.circle(resized_frame, ball_center, 5, draw_colour[colour], -1)
 
             # Reposition centre so (0, 0) is in the middle for the user.
             # Keep scale as 1 as user sees scaled down 320x240 image in Further
             ball_center = centroid_reposition(ball_center, 1, resized_frame)
             ball_angle = get_control_angle(ball_center, resized_frame)
+
+            ball_centers[colour] = ball_center
+            ball_radii[colour] = ball_radius
+            ball_angles[colour] = ball_angle
+            ball_finds[colour] = True
+        else:
+            ball_centers[colour] = None
+            ball_radii[colour] = None
+            ball_angles[colour] = None
+            ball_finds[colour] = False
 
         for i in range(1, len(detection_points[colour])):
             # if either of the tracked points are None, ignore them
@@ -155,11 +166,7 @@ def process_frame_for_ball(frame, colours=("red",), image_return_format: str = "
             thickness = int(np.sqrt(BUFFER_LENGTH / float(i + 1)))
 
             cv2.line(resized_frame, detection_points[colour][i - 1], detection_points[colour][i],
-                     draw_colour[colours], thickness)
-
-        ball_centers[colour] = ball_center
-        ball_radii[colour] = ball_radius
-        ball_angles[colour] = ball_angle
+                     draw_colour[colour], thickness)
 
     if image_return_format.lower() != 'opencv':
         robot_view = ImageFunctions.convert(resized_frame, format="PIL")
@@ -169,18 +176,56 @@ def process_frame_for_ball(frame, colours=("red",), image_return_format: str = "
     if len(colours) == 1:
         colour = colours[0]
         return DotDict({
+            "found": ball_finds[colour],
             "center": ball_centers[colour],
             "robot_view": robot_view,
             "radius": ball_radii[colour],
-            "angle": ball_angles[colour]
+            "angle": ball_angles[colour],
+            colour: DotDict({
+                "found": ball_finds[colour],
+                "center": ball_centers[colour],
+                "robot_view": robot_view,
+                "radius": ball_radii[colour],
+                "angle": ball_angles[colour]
+            }),
         })
     elif len(colours) == 2:
         return DotDict({
+            "robot_view": robot_view,
             colours[0]: DotDict({
-                        "center": ball_centers[colours[0]],
-                        "robot_view": robot_view,
-                        "radius": ball_radii[colours[0]],
-                        "angle": ball_angles[colours[0]]
+                "found": ball_finds[colours[0]],
+                "center": ball_centers[colours[0]],
+                "radius": ball_radii[colours[0]],
+                "angle": ball_angles[colours[0]]
+            }),
+            colours[1]: DotDict({
+                "found": ball_finds[colours[1]],
+                "center": ball_centers[colours[1]],
+                "radius": ball_radii[colours[1]],
+                "angle": ball_angles[colours[1]]
+            })
+
+        })
+    elif len(colours) == 3:
+        return DotDict({
+            "robot_view": robot_view,
+            colours[0]: DotDict({
+                "found": ball_finds[colours[0]],
+                "center": ball_centers[colours[0]],
+                "radius": ball_radii[colours[0]],
+                "angle": ball_angles[colours[0]]
+            }),
+            colours[1]: DotDict({
+                "found": ball_finds[colours[1]],
+                "center": ball_centers[colours[1]],
+                "radius": ball_radii[colours[1]],
+                "angle": ball_angles[colours[1]]
+            }),
+            colours[2]: DotDict({
+                "found": ball_finds[colours[2]],
+                "center": ball_centers[colours[2]],
+                "radius": ball_radii[colours[2]],
+                "angle": ball_angles[colours[2]]
             })
 
         })
