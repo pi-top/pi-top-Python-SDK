62f2fec32b889197d87a7dc5538742ae8cf3d470
Ryan Dunwoody <me@ryandunwoody.com>
2021-03-26 18:39:16 +0800

    Add face detector example

---
+++ examples/camera/camera_face_detector.py
@@ -0,0 +1,25 @@
+from pitop import Camera
+from pitop.processing.algorithms import FaceDetector
+from signal import pause
+import cv2
+
+
+def find_faces(frame):
+    face = face_detector.detect(frame)
+    robot_view = face.robot_view
+
+    cv2.imshow("Faces", robot_view)
+    cv2.waitKey(1)
+
+    if face.found:
+        print(f"Face center: {face.center}")
+    else:
+        print("Cannot find face!")
+
+
+camera = Camera(format="OpenCV", rotate_angle=180)
+face_detector = FaceDetector()
+
+camera.on_frame = find_faces
+
+pause()
