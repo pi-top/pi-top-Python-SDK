0820c2ae019276f2f0e77d86f85ca6fe3c2fa8c2
Ryan Dunwoody <me@ryandunwoody.com>
2021-03-26 14:43:09 +0800

    Delete localization code

--- pitop/processing/algorithms/__init__.py
+++ pitop/processing/algorithms/__init__.py
@@ -1,5 +1,3 @@
 from .line_detect import process_frame_for_line
-from .aruco_detector import ArucoMarkers
 from .ball_detect import process_frame_for_ball
 from .ball_detect import colour_filter
-from .extended_kalman_filter import EKF
