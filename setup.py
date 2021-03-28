d29040a83c26d3caa87e79334e3d9202a375298b
Jorge Capona <jcapona@users.noreply.github.com>
2021-03-27 20:43:43 -0700

    Re-add tests (#275)

--- setup.py
+++ setup.py
@@ -128,7 +128,6 @@ __requires__ = [
 __extra_requires__ = {
     "computer_vision": ["opencv"],
     "doc": ["sphinx"],
-    "test": ["pytest", "coverage", "mock", "pytest-cov", "pytest-mock"],
 }
 
 __entry_points__ = {
