bba27968567b53c786c0008739e5ef284612db81
Ryan Dunwoody <me@ryandunwoody.com>
2021-03-29 17:02:10 +0800

    Add another implementation of face angle retrieval
    Use central points on face

--- pitop/processing/algorithms/faces/face_utils.py
+++ pitop/processing/algorithms/faces/face_utils.py
@@ -9,11 +9,13 @@ def get_face_angle(face_features):
     :param face_features: dlib face features
     :return: angle of face in degrees
     """
-    left_start, left_end = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
-    right_start, right_end = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+    left_eye_start, left_eye_end = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+    right_eye_start, right_eye_end = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
-    left_eye = face_features[left_start:left_end]
-    right_eye = face_features[right_start:right_end]
+    jaw_start, jaw_end = face_utils.FACIAL_LANDMARKS_68_IDXS["jaw"]
+
+    left_eye = face_features[left_eye_start:left_eye_end]
+    right_eye = face_features[right_eye_start:right_eye_end]
 
     left_eye_position = np.average(left_eye, axis=0)
     right_eye_position = np.average(right_eye, axis=0)
@@ -23,5 +25,32 @@ def get_face_angle(face_features):
     x_diff, y_diff = position_diff
 
     angle = math.degrees(math.atan(y_diff/x_diff))
+
+    return angle
+
+
+central_face_features = (27, 28, 29, 30, 33, 51, 62, 66, 57, 8)
+
+
+def get_face_angle_2(face_features):
+    """
+
+    :param face_features:
+    :return:
+    """
+    central_points = np.take(face_features, central_face_features, axis=0)
+    x = central_points[:, 0]
+    y = central_points[:, 1]
+
+    A = np.vstack([x, np.ones(len(x))]).T
+
+    m, c = np.linalg.lstsq(A, y, rcond=None)[0]
+
+    angle = math.degrees(math.atan(-m))
+
+    if angle > 0:
+        angle = 90 - angle
+    else:
+        angle = -90 - angle
 
     return angle
