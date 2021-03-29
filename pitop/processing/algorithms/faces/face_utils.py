e130e402d3f96974e4dfb188267c2c33b227e2aa
Ryan Dunwoody <me@ryandunwoody.com>
2021-03-29 17:29:04 +0800

    Add support for dlib's 5 landmark face feature predictor

--- pitop/processing/algorithms/faces/face_utils.py
+++ pitop/processing/algorithms/faces/face_utils.py
@@ -9,27 +9,37 @@ def get_face_angle(face_features):
     :param face_features: dlib face features
     :return: angle of face in degrees
     """
-    left_eye_start, left_eye_end = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
-    right_eye_start, right_eye_end = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
-    left_eyebrow_start, left_eyebrow_end = face_utils.FACIAL_LANDMARKS_IDXS["left_eyebrow"]
-    right_eyebrow_start, right_eyebrow_end = face_utils.FACIAL_LANDMARKS_IDXS["right_eyebrow"]
-    jaw_start, jaw_end = face_utils.FACIAL_LANDMARKS_68_IDXS["jaw"]
+    if len(face_features) == 68:
+        left_eye_start, left_eye_end = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
+        right_eye_start, right_eye_end = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
+        left_eyebrow_start, left_eyebrow_end = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eyebrow"]
+        right_eyebrow_start, right_eyebrow_end = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eyebrow"]
+        jaw_start, jaw_end = face_utils.FACIAL_LANDMARKS_68_IDXS["jaw"]
 
-    left_eye = face_features[left_eye_start:left_eye_end]
-    right_eye = face_features[right_eye_start:right_eye_end]
+        left_eye = face_features[left_eye_start:left_eye_end]
+        right_eye = face_features[right_eye_start:right_eye_end]
 
-    left_eyebrow = face_features[left_eyebrow_start: left_eyebrow_end]
-    right_eyebrow = face_features[right_eyebrow_start: right_eyebrow_end]
+        left_eyebrow = face_features[left_eyebrow_start: left_eyebrow_end]
+        right_eyebrow = face_features[right_eyebrow_start: right_eyebrow_end]
 
-    jaw = face_features[jaw_start:jaw_end]
-    left_jaw = jaw[10:18]
-    right_jaw = jaw[0:9]
+        jaw = face_features[jaw_start:jaw_end]
+        left_jaw = jaw[10:18]
+        right_jaw = jaw[0:9]
 
-    left_mouth = np.take(face_features, (52, 53, 54, 55, 56, 63, 64, 65), axis=0)
-    right_mouth = np.take(face_features, (48, 49, 50, 58, 59, 60, 61, 67), axis=0)
+        left_mouth = np.take(face_features, (52, 53, 54, 55, 56, 63, 64, 65), axis=0)
+        right_mouth = np.take(face_features, (48, 49, 50, 58, 59, 60, 61, 67), axis=0)
 
-    all_left_points = np.vstack((left_eye, left_eyebrow, left_jaw, left_mouth))
-    all_right_points = np.vstack((right_eye, right_eyebrow, right_jaw, right_mouth))
+        all_left_points = np.vstack((left_eye, left_eyebrow, left_jaw, left_mouth))
+        all_right_points = np.vstack((right_eye, right_eyebrow, right_jaw, right_mouth))
+
+    elif len(face_features) == 5:
+        left_eye_start, left_eye_end = face_utils.FACIAL_LANDMARKS_5_IDXS["left_eye"]
+        right_eye_start, right_eye_end = face_utils.FACIAL_LANDMARKS_5_IDXS["right_eye"]
+        all_left_points = face_features[left_eye_start:left_eye_end]
+        all_right_points = face_features[right_eye_start:right_eye_end]
+
+    else:
+        raise ValueError("dlib face features not recognised, please try again")
 
     left_centroid = np.average(all_left_points, axis=0)
     right_centroid = np.average(all_right_points, axis=0)
