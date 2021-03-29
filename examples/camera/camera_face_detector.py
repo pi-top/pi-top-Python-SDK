19186e0fd451cb15fb42d255ed7ebdf6a6445ea5
Ryan Dunwoody <me@ryandunwoody.com>
2021-03-29 17:22:36 +0800

    Refactor

--- examples/camera/camera_face_detector.py
+++ examples/camera/camera_face_detector.py
@@ -1,5 +1,5 @@
 from pitop import Camera
-from pitop.processing.algorithms import FaceDetectorDLib
+from pitop.processing.algorithms.faces import FaceDetector
 from signal import pause
 import cv2
 
@@ -12,13 +12,13 @@ def find_faces(frame):
     cv2.waitKey(1)
 
     if face.found:
-        print(f"Face center: {face.center}")
+        print(f"Face angle: {face.angle}")
     else:
         print("Cannot find face!")
 
 
 camera = Camera(format="OpenCV", rotate_angle=180)
-face_detector = FaceDetectorDLib(input_format="OpenCV", output_format="OpenCV")
+face_detector = FaceDetector(input_format="OpenCV", output_format="OpenCV")
 
 camera.on_frame = find_faces
 
