f520f8ff45d18d464a513fc5e871af388404615d
Ryan Dunwoody <me@ryandunwoody.com>
2021-03-31 21:11:44 +0800

    Add simple preview and image capture example for testing

---
+++ examples/camera/camera_image_preview_capture.py
@@ -0,0 +1,25 @@
+from pitop import Pitop, Camera
+import cv2
+from time import sleep
+
+miniscreen = Pitop().miniscreen
+
+cam = Camera(format="OpenCV", flip_top_bottom=True)
+directory = "images/"
+button = miniscreen.select_button
+
+picture_count = 0
+
+while True:
+    frame = cam.get_frame()
+
+    cv2.imshow("Frame", frame)
+    if button.is_pressed:
+        cv2.imwrite(f'{directory}image_{picture_count}.jpg', frame)
+        print(f"Frame written to file with ID: {picture_count}\n")
+        picture_count += 1
+        sleep(0.5)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
