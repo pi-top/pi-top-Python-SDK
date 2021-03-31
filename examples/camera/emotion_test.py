2df2239db9b6647595175737cab6c13987204328
Ryan Dunwoody <me@ryandunwoody.com>
2021-03-31 21:12:54 +0800

    Add emotion test example
    Will be deleted later

--- examples/camera/emotion_test.py
+++ examples/camera/emotion_test.py
@@ -1,19 +1,18 @@
-from pitop.processing.algorithms.faces import FaceDetector, FaceDetector2, emotion_classifier
+from pitop.processing.algorithms.faces import FaceDetector, emotion_classifier
 import cv2
-from imutils.convenience import resize
-
-test_image = cv2.imread("images/test_1.jpg")
-# test_image = cv2.imread("test_image.png")
 
 face_detector = FaceDetector(input_format="OpenCV", output_format="OpenCV")
-# face_detector = FaceDetector2()
 
-face = face_detector.detect(test_image)
+count = 0
+while count < 7:
+    test_image = cv2.imread(f"images/test_{count}.jpg")
 
-prediction = emotion_classifier(face.features, face.dimensions)
+    face = face_detector.detect(test_image)
+    if face.found:
+        emotion = emotion_classifier(face.features, face.dimensions)
+        print(emotion.type, emotion.confidence)
+        cv2.imshow("robotview", face.robot_view)
+        cv2.waitKey(0)
+    count += 1
 
-print(prediction)
-
-# cv2.imshow("robotview", face.robot_view)
-cv2.imshow("robotview", resize(face.robot_view, width=320))
-cv2.waitKey(0)
+cv2.destroyAllWindows()
