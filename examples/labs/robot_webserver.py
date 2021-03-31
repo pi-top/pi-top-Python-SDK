e6f4263b65c2926eabb9267227623b4cbe99899a
Jorge Capona <jcapona@gmail.com>
2021-03-31 13:02:04 -0300

    Update example

--- examples/labs/robot_webserver.py
+++ examples/labs/robot_webserver.py
@@ -1,11 +1,21 @@
-from pitop import AlexRobot
+from pitop import Pitop, DriveController, PanTiltController, Camera
 from pitop.labs.webserver import run_webserver
 
-alex = AlexRobot()
-alex.calibrate()
-alex.pan_servo.target_angle = 0
-alex.tilt_servo.target_angle = 0
 
-run_webserver(robot_instance=alex,
+# Create components
+robot = Pitop()
+drive_controller = DriveController()
+pan_tilt = PanTiltController()
+camera = Camera()
+
+
+# Add components to the robot object
+robot.add_component(drive_controller)
+robot.add_component(pan_tilt)
+robot.add_component(camera)
+
+
+# Run webserver
+run_webserver(robot_instance=robot,
               port=8070,
               serve_forever=True)
