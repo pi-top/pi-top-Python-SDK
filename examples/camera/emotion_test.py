cb51e5b655b68576aeed2c0cc0ee983d1bf28701
Ryan Dunwoody <me@ryandunwoody.com>
2021-04-06 17:52:13 +0800

    Update for new test images

--- examples/camera/emotion_test.py
+++ examples/camera/emotion_test.py
@@ -1,17 +1,19 @@
-from pitop.processing.algorithms.faces import FaceDetector
+from pitop.processing.algorithms.faces import FaceDetector, EmotionDetector
 import cv2
 
 face_detector = FaceDetector(input_format="OpenCV", output_format="OpenCV")
+emotion_detector = EmotionDetector(input_format="OpenCV", output_format="OpenCV")
 
 count = 0
-while count < 7:
-    test_image = cv2.imread(f"images/test_{count}.jpg")
+while count < 5:
+    test_image = cv2.imread(f"images/image_{count}.jpg")
 
     face = face_detector.detect(test_image)
+    emotion = emotion_detector.detect(face)
     if face.found:
-        emotion = face.emotion
         print(emotion.type, emotion.confidence)
-        cv2.imshow("robotview", face.robot_view)
+        cv2.imshow("face", face.robot_view)
+        cv2.imshow("emotion", emotion.robot_view)
         cv2.waitKey(0)
     count += 1
 
