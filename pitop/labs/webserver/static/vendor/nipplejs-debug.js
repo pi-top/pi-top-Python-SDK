1347d2ad57b6441b321feac9a30fc079785d388b
Ryan Dunwoody <me@ryandunwoody.com>
2021-04-13 23:06:05 +0800

    Revert js change

--- pitop/labs/webserver/static/vendor/nipplejs-debug.js
+++ pitop/labs/webserver/static/vendor/nipplejs-debug.js
@@ -1,7 +1,7 @@
 // Source: https://raw.githubusercontent.com/yoannmoinet/nipplejs/5a8f8bacea3776630a270a25dbbd760c9451bb55/src/nipple.js
 
 import Super from './super';
-import * as u from './core';
+import * as u from './utils';
 
 ///////////////////////
 ///   THE NIPPLE    ///
