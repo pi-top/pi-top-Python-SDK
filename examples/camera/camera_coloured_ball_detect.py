a0f161f91489f9ddfc3ed1837b203d805dc9b431
Ryan Dunwoody <me@ryandunwoody.com>
2021-04-14 20:10:37 +0800

    Change print new line

--- examples/camera/camera_coloured_ball_detect.py
+++ examples/camera/camera_coloured_ball_detect.py
@@ -11,19 +11,22 @@ def process_frame(frame):
     if red_ball.found:
         print(f'Red ball center: {red_ball.center}')
         print(f'Red ball radius: {red_ball.radius}')
-        print(f'Red ball angle: {red_ball.angle}\n')
+        print(f'Red ball angle: {red_ball.angle}')
+        print()
 
     green_ball = detected_balls.green
     if green_ball.found:
         print(f'Green ball center: {green_ball.center}')
         print(f'Green ball radius: {green_ball.radius}')
-        print(f'Green ball angle: {green_ball.angle}\n')
+        print(f'Green ball angle: {green_ball.angle}')
+        print()
 
     blue_ball = detected_balls.blue
     if blue_ball.found:
         print(f'Blue ball center: {blue_ball.center}')
         print(f'Blue ball radius: {blue_ball.radius}')
-        print(f'Blue ball angle: {blue_ball.angle}\n')
+        print(f'Blue ball angle: {blue_ball.angle}')
+        print()
 
     cv2.imshow("Image", detected_balls.robot_view)
     cv2.waitKey(1)
