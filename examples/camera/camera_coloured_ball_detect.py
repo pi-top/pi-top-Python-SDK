51f8076e1e16381b78f58ccda3fd47b0f71cee0e
Ryan Dunwoody <me@ryandunwoody.com>
2021-04-14 16:14:15 +0800

    Change camera resolution and add more printouts

--- examples/camera/camera_coloured_ball_detect.py
+++ examples/camera/camera_coloured_ball_detect.py
@@ -10,21 +10,27 @@ def process_frame(frame):
     red_ball = detected_balls.red
     if red_ball.is_valid():
         print(f'Red ball center: {red_ball.center}')
+        print(f'Red ball radius: {red_ball.radius}')
+        print(f'Red ball angle: {red_ball.angle}\n')
 
     green_ball = detected_balls.green
     if green_ball.is_valid():
         print(f'Green ball center: {green_ball.center}')
+        print(f'Green ball radius: {green_ball.radius}')
+        print(f'Green ball angle: {green_ball.angle}\n')
 
     blue_ball = detected_balls.blue
     if blue_ball.is_valid():
-        print(f'Blue ball center: {blue_ball.center}\n')
+        print(f'Blue ball center: {blue_ball.center}')
+        print(f'Blue ball radius: {blue_ball.radius}')
+        print(f'Blue ball angle: {blue_ball.angle}\n')
 
     cv2.imshow("Image", detected_balls.robot_view)
     cv2.waitKey(1)
 
 
 ball_detector = BallDetector()
-camera = Camera()
+camera = Camera(resolution=(640, 480))
 camera.on_frame = process_frame
 
 pause()
