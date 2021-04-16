8c9b67629f4b790482a24df351ddcf8d8feb97da
Ryan Dunwoody <me@ryandunwoody.com>
2021-04-16 16:55:12 +0800

    Fix example for new class APIs

--- examples/camera/camera_emotion_detector.py
+++ examples/camera/camera_emotion_detector.py
@@ -1,27 +1,20 @@
-from os import environ
-from pitopcommon.current_session_info import get_first_display
 from pitop import Camera
 from pitop.processing.algorithms.faces import (
     FaceDetector,
     EmotionDetector
 )
 from signal import pause
-from pitop.processing.core.vision_functions import import_opencv
+import cv2
 
 
-cv2 = import_opencv()
-
-environ["DISPLAY"] = get_first_display()
-
-
-def frame_callback(frame):
+def detect_emotion(frame):
     face = face_detector.detect(frame)
     emotion = emotion_detector.detect(face)
 
     cv2.imshow("Emotion", emotion.robot_view)
     cv2.waitKey(1)
 
-    if face.found:
+    if emotion.found:
         if emotion.type == "Happy":
             print(":)")
         elif emotion.type == "Sad":
@@ -38,11 +31,11 @@ def frame_callback(frame):
         print("Face not found")
 
 
-camera = Camera(format="OpenCV", flip_top_bottom=True)
+camera = Camera(flip_top_bottom=True)
 
-face_detector = FaceDetector(input_format="OpenCV", format="OpenCV")
-emotion_detector = EmotionDetector(input_format="OpenCV", format="OpenCV")
+face_detector = FaceDetector()
+emotion_detector = EmotionDetector()
 
-camera.on_frame = frame_callback
+camera.on_frame = detect_emotion
 
 pause()
