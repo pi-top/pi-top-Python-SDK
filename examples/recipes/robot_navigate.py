188abb6aa7be48392acba688b272ee8a76465717
Ryan Dunwoody <me@ryandunwoody.com>
2021-04-21 21:19:18 +0800

    Add example for driving in a square

--- examples/recipes/robot_navigate.py
+++ examples/recipes/robot_navigate.py
@@ -1,8 +1,22 @@
 from pitop import DriveController, NavigationController
+from signal import pause
 
 drive_controller = DriveController(left_motor_port="M3", right_motor_port="M0")
 
 navigation_controller = NavigationController(drive_controller=drive_controller)
 
 while True:
-    navigation_controller.go_to()
+    navigation_controller.go_to(x_goal=1, y_goal=0)
+    print(f"finished with state: {navigation_controller._robot_state}")
+
+    navigation_controller.go_to(x_goal=1, y_goal=1)
+    print(f"finished with state: {navigation_controller._robot_state}")
+
+    navigation_controller.go_to(x_goal=0, y_goal=1)
+    print(f"finished with state: {navigation_controller._robot_state}")
+
+    navigation_controller.go_to(x_goal=0, y_goal=0, theta_goal=0)
+    print(f"finished with state: {navigation_controller._robot_state}")
+
+# drive_controller.robot_move(linear_speed=0, angular_speed=1)
+# pause()
