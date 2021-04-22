ea45d0497bdcc530b4eb4251a98178a059046d5a
Ryan Dunwoody <me@ryandunwoody.com>
2021-04-22 11:28:14 +0800

    Add goal criteria and fix overshoot issue

--- pitop/robotics/navigation/navigation_controller.py
+++ pitop/robotics/navigation/navigation_controller.py
@@ -2,7 +2,6 @@ from threading import Thread, Event
 from time import time
 from dataclasses import dataclass
 import numpy as np
-import matplotlib.pyplot as plt
 from random import random
 import math
 from simple_pid import PID
@@ -29,14 +28,45 @@ class RobotState:
         return math.hypot(dx, dy)
 
 
+class GoalCriteria:
+    def __init__(self):
+        self._MAX_GOAL_REACHED_DISTANCE_ERROR = 0.0
+        self._MAX_GOAL_REACHED_ANGLE_ERROR = 0.0
+
+    def set(self, linear_speed_factor, angular_speed_factor):
+        self._MAX_GOAL_REACHED_DISTANCE_ERROR = linear_speed_factor * 0.02  # 2 mm at full speed
+        self._MAX_GOAL_REACHED_ANGLE_ERROR = angular_speed_factor * math.radians(4.0)  # 4deg at full speed
+        return self
+
+    def angle(self, angle_error):
+        if abs(angle_error) < self._MAX_GOAL_REACHED_ANGLE_ERROR:
+            return True
+        return False
+
+    def distance(self, distance_error):
+        if abs(distance_error) < self._MAX_GOAL_REACHED_DISTANCE_ERROR:
+            return True
+        return False
+
+
 class NavigationController:
-    _DECELERATION_DISTANCE = 0.2
+    _DECELERATION_DISTANCE = 0.2  # m
     _DECELERATION_ANGLE = math.radians(30.0)
 
-    def __init__(self, drive_controller=None,
-                 linear_speed_factor: float = 0.5,
-                 angular_speed_factor: float = 0.25,
+    _MAX_GOAL_REACHED_ANGLE_ERROR = math.radians(1.0)
+    _MAX_GOAL_REACHED_DISTANCE_ERROR = 0.01  # m
+
+    def __init__(self,
+                 drive_controller=None,
+                 linear_speed_factor: float = 0.75,
+                 angular_speed_factor: float = 0.5,
                  sim=False):
+
+        self._robot_state = RobotState()
+        self._odom_update_frequency = 10.0
+        self._position_update_event = Event()
+        self._odometry_tracker = Thread(target=self.__track_odometry, daemon=True)
+        self._odometry_tracker.start()
 
         self._sim = sim
         if not self._sim:
@@ -45,14 +75,18 @@ class NavigationController:
             self._max_robot_angular_speed = self._drive_controller.max_robot_angular_speed
             self._linear_speed_factor = linear_speed_factor
             self._angular_speed_factor = angular_speed_factor
+
             self._max_linear_speed = self._linear_speed_factor * self._max_motor_speed
             self._max_angular_speed = self._angular_speed_factor * self._max_robot_angular_speed
+            self._DECELERATION_DISTANCE = self._linear_speed_factor * 0.4  # 0.4m at full speed
+            self._DECELERATION_ANGLE = self._angular_speed_factor * math.radians(120.0)  # 120deg at full speed
 
-        self._odom_update_frequency = 10.0
-        self._position_update_event = Event()
-        self._odometry_tracker = Thread(target=self.__track_odometry, daemon=True)
-        self._odometry_tracker.start()
-        self._robot_state = RobotState()
+            self._goal_criteria = GoalCriteria().set(linear_speed_factor=linear_speed_factor,
+                                                     angular_speed_factor=angular_speed_factor
+                                                     )
+
+            # self._MAX_GOAL_REACHED_DISTANCE_ERROR = self._linear_speed_factor * 0.02  # 2 mm at full speed
+            # self._MAX_GOAL_REACHED_ANGLE_ERROR = self._angular_speed_factor * math.radians(4.0)  # 4deg at full speed
 
         self._heading_pid = PID(Kp=1.0 / self._DECELERATION_ANGLE,
                                 Ki=0.1,
@@ -67,19 +101,17 @@ class NavigationController:
                                  output_limits=(-1.0, 1.0)
                                  )
 
-    def go_to(self, x_goal: float, y_goal: float, theta_goal=None):
+    def go_to(self, x: float, y: float, theta=None):
 
-        if x_goal is not None and y_goal is not None:
-            self.__set_course_heading(x_goal=x_goal, y_goal=y_goal)
-            self.__drive_to_goal(x_goal=x_goal, y_goal=y_goal)
-        if theta_goal is not None:
-            self.__rotate_to_theta_goal(theta_goal=theta_goal)
+        if x is not None and y is not None:
+            self.__set_course_heading(x_goal=x, y_goal=y)
+            self.__drive_to_goal(x_goal=x, y_goal=y)
+        if theta is not None:
+            self.__rotate_to_theta_goal(theta_goal=theta)
 
     def __set_course_heading(self, x_goal, y_goal):
         while True:
-            self._position_update_event.wait()
-            self._position_update_event.clear()
-            x, y, theta = self._robot_state.pose
+            x, y, theta = self.__get_new_pose_update()
 
             x_diff = x_goal - x
             y_diff = y_goal - y
@@ -87,31 +119,32 @@ class NavigationController:
             heading_error = self.__normalize_angle(theta - math.atan2(y_diff, x_diff))
             angular_speed = self.__get_angular_speed(heading_error=heading_error)
 
-            if abs(heading_error) < np.radians(1.0):  # and abs(angular_speed < 0.1):
-                print("Heading goal reached")
+            if self._goal_criteria.angle(heading_error):
                 self.__goal_reached()
                 break
 
             self.robot_move(linear_speed=0, angular_speed=angular_speed)
 
     def __drive_to_goal(self, x_goal, y_goal):
-
         while True:
-            self._position_update_event.wait()
-            self._position_update_event.clear()
-            x, y, theta = self._robot_state.pose
+            x, y, theta = self.__get_new_pose_update()
 
             x_diff = x_goal - x
             y_diff = y_goal - y
 
-            heading_error = self.__normalize_angle(theta - math.atan2(y_diff, x_diff))
-            angular_speed = self.__get_angular_speed(heading_error=heading_error)
-
+            heading_error = self.__get_angle_error(current_angle=theta, target_angle=math.atan2(y_diff, x_diff))
             distance_error = -np.hypot(x_diff, y_diff)
+            print(f"heading_error: {math.degrees(heading_error)}")
+
+            if abs(heading_error) > math.pi / 2:
+                # Overshot goal, reverse. Heading error set to zero as angle variance is huge when x and y are small
+                distance_error = -distance_error * 1.5  # slow to correct from overshoot, give it a boost
+                heading_error = 0
+
+            angular_speed = self.__get_angular_speed(heading_error=heading_error)
             linear_speed = self.__get_linear_speed(distance_error=distance_error)
 
-            if abs(distance_error) < 0.01:
-                print("Position goal reached")
+            if self._goal_criteria.distance(distance_error):
                 self.__goal_reached()
                 break
 
@@ -119,15 +152,12 @@ class NavigationController:
 
     def __rotate_to_theta_goal(self, theta_goal):
         while True:
-            self._position_update_event.wait()
-            self._position_update_event.clear()
+            x, y, theta = self.__get_new_pose_update()
 
-            theta = self._robot_state.theta
-            heading_error = self.__normalize_angle(theta - theta_goal)
+            heading_error = self.__get_angle_error(current_angle=theta, target_angle=theta_goal)
             angular_speed = self.__get_angular_speed(heading_error=heading_error)
 
-            if abs(heading_error) < np.radians(1.0):  # and abs(angular_speed < 0.1):
-                print("Theta goal reached")
+            if self._goal_criteria.angle(heading_error):
                 self.__goal_reached()
                 break
 
@@ -136,8 +166,17 @@ class NavigationController:
     def __goal_reached(self):
         self.robot_stop()
         self._heading_pid.reset()
+        # wait for one more pose update before returning to ensure subsequent code has latest (could be user code)
         self._position_update_event.wait()
         self._position_update_event.clear()
+
+    def __get_new_pose_update(self):
+        self._position_update_event.wait()
+        self._position_update_event.clear()
+        return self._robot_state.pose
+
+    def __get_angle_error(self, current_angle, target_angle):
+        return self.__normalize_angle(current_angle - target_angle)
 
     @staticmethod
     def __normalize_angle(angle):
@@ -155,7 +194,6 @@ class NavigationController:
         return self._max_linear_speed * self._position_pid(distance_error)
 
     def __track_odometry(self):
-
         prev_time = time()
         while True:
             current_time = time()
