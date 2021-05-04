76dc27dc462f9de710224110537462d9b22080ac
olivierwilkinson <olivier.wilkinson@gmail.com>
2021-05-04 09:46:42 +0100

    Expose default rover controller message handlers
    Currently users would need to fully reimplement the handler from scratch
    to customise it's behaviour. Exposing the default handlers as methods
    means users can use them in their custom handlers.
    
    Add an example to rover_controller to show how you would swap joysticks
    using the exposed handlers.

---
+++ examples/labs/rover_controller/swap_joysticks.py
@@ -0,0 +1,18 @@
+from pitop import Pitop, DriveController, Camera, PanTiltController
+from pitop.labs import RoverWebController
+from pitop.labs.web.blueprints.rover import pan_tilt_handler, drive_handler
+
+rover = Pitop()
+rover.add_component(DriveController())
+rover.add_component(PanTiltController())
+rover.add_component(Camera())
+
+rover_controller = RoverWebController(
+    get_frame=rover.camera.get_frame,
+    message_handlers={
+        'left_joystick': lambda data: drive_handler(rover.drive, data),
+        'right_joystick': lambda data: pan_tilt_handler(rover.pan_tilt, data)
+    }
+)
+
+rover_controller.serve_forever()
--- pitop/labs/web/blueprints/rover/__init__.py
+++ pitop/labs/web/blueprints/rover/__init__.py
@@ -4,6 +4,21 @@ from pitop.labs.web.blueprints.controller import ControllerBlueprint
 from .helpers import calculate_velocity_twist, calculate_pan_tilt_angle
 
 
+def drive_handler(drive, data):
+    velocity_twist = calculate_velocity_twist(data)
+    linear = velocity_twist.get('linear', 0)
+    angular = velocity_twist.get('angular', 0)
+
+    drive.robot_move(linear, angular)
+
+
+def pan_tilt_handler(pan_tilt, data):
+    angle = calculate_pan_tilt_angle(data)
+
+    pan_tilt.pan_servo.target_angle = angle.get('z')
+    pan_tilt.tilt_servo.target_angle = angle.get('y')
+
+
 class RoverControllerBlueprint(Blueprint):
     def __init__(
         self,
@@ -21,21 +36,20 @@ class RoverControllerBlueprint(Blueprint):
             **kwargs
         )
 
-        self.drive = drive
-        self.pan_tilt = pan_tilt
-
         if message_handlers.get('left_joystick') is None:
-            message_handlers['left_joystick'] = self.left_joystick
+            def left_joystick(data): return drive_handler(drive, data)
+            message_handlers['left_joystick'] = left_joystick
 
         if message_handlers.get('right_joystick') is None:
-            message_handlers['right_joystick'] = self.right_joystick
+            def right_joystick(data): return pan_tilt_handler(pan_tilt, data)
+            message_handlers['right_joystick'] = right_joystick
 
         # set show_left_joystick every request for use in base_rover.html
         @self.before_app_request
         def set_show_left_joystick():
             g.show_left_joystick = (
                 message_handlers.get('left_joystick') is not None
-                or self.pan_tilt is not None
+                or pan_tilt is not None
             )
 
         self.controller_blueprint = ControllerBlueprint(
@@ -44,19 +58,6 @@ class RoverControllerBlueprint(Blueprint):
     def register(self, app, options, *args, **kwargs):
         app.register_blueprint(self.controller_blueprint, **options)
         Blueprint.register(self, app, options, *args, **kwargs)
-
-    def right_joystick(self, data):
-        velocity_twist = calculate_velocity_twist(data)
-        linear = velocity_twist.get('linear', 0)
-        angular = velocity_twist.get('angular', 0)
-
-        self.drive.robot_move(linear, angular)
-
-    def left_joystick(self, data):
-        angle = calculate_pan_tilt_angle(data)
-
-        self.pan_tilt.pan_servo.target_angle = angle.get('z')
-        self.pan_tilt.tilt_servo.target_angle = angle.get('y')
 
     def broadcast(self, message):
         self.controller_blueprint.broadcast(message)
