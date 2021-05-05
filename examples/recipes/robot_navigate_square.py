172333098e8d378738941e4641b8a32ca0aeb936
Ryan Dunwoody <me@ryandunwoody.com>
2021-05-05 19:30:31 +0800

    Update example

--- examples/recipes/robot_navigate_square.py
+++ examples/recipes/robot_navigate_square.py
@@ -1,7 +1,11 @@
-from pitop import DriveController, NavigationController, IMU
+"""Demo of how to use the Navigation Controller to control a robot. Place your robot (preferably the Alex or Bobbie
+configuration) in the middle of a 1x1 meter square and run the code - the robot will drive around the square twice and
+then back to the middle. The Ultrasonic Sensor is also used to detect obstacles during navigation. If one is found,
+the robot will move onto the next navigation goal. """
+
+from pitop import DriveController, NavigationController
 from pitop.pma import UltrasonicSensor
 from time import sleep
-import numpy as np
 
 
 def navigation_finished():
@@ -13,7 +17,7 @@ def navigation_finished():
 def check_for_obstacles():
     while not goal_reached:
         sleep(0.15)
-        if ultrasonic.distance < 0.25:
+        if ultrasonic.distance < 0.2:
             print("Obstacle blocking navigation goal, changing to next goal!")
             navigate.stop()
             sleep(1)
@@ -23,8 +27,8 @@ def check_for_obstacles():
 def set_goal_with_obstacle_detection(position, angle=None):
     global goal_reached
     goal_reached = False
-    navigate.go_to(position=position, angle=angle, on_finish=navigation_finished).wait()
-    # check_for_obstacles()
+    navigate.go_to(position=position, angle=angle, on_finish=navigation_finished)
+    check_for_obstacles()
 
 
 def main():
@@ -42,34 +46,13 @@ def main():
 
     # go back to start_position
     navigate.go_to(position=(0, 0), angle=0, on_finish=navigation_finished, backwards=True).wait()
-    # navigate.robot_state._saver.to_array()
-    # x = navigate.robot_state._saver.x
-    # x_prior = navigate.robot_state._saver.x_prior
-    # P_prior = navigate.robot_state._saver.P_prior
-    # P = navigate.robot_state._saver.P
-    # z = navigate.robot_state._saver.z
-    # y = navigate.robot_state._saver.y
-    #
-    # with open(r"nav_data.npy", "wb") as f:
-    #     np.save(f, x)
-    #     np.save(f, x_prior)
-    #     np.save(f, P_prior)
-    #     np.save(f, P)
-    #     np.save(f, z)
-    #     np.save(f, y)
-
-    # with open(r"nav_data_x.pickle", "wb") as output_file:
-    #     pickle.dump(x, output_file)
-    #
-    # with open(r"nav_data_x.pickle", "wb") as output_file:
-    #     pickle.dump(x, output_file)
+    print(f"Goal reached with state: \n{navigate.robot_state}")
 
 
 goal_reached = False
 navigate = NavigationController(
-    drive_controller=DriveController(left_motor_port="M3", right_motor_port="M0"),
-    imu=IMU()
+    drive_controller=DriveController(left_motor_port="M3", right_motor_port="M0")
 )
-# ultrasonic = UltrasonicSensor("D3")
+ultrasonic = UltrasonicSensor("D3")
 
 main()
