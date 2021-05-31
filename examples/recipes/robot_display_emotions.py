37ae6e17523ca7eb7e86ae65c1f2b349c5eb6cfc
Jorge Capona <jcapona@gmail.com>
2021-05-31 19:11:19 +0100

    Refactor robot_display_emotions example

--- examples/recipes/robot_display_emotions.py
+++ examples/recipes/robot_display_emotions.py
@@ -28,93 +28,53 @@ emoji_unicodes = {
 
 text_font = ImageFont.truetype("VeraMono.ttf", size=12)
 emoji_font = ImageFont.truetype("Symbola_hint.ttf", size=50, encoding='unic')
-looking_for_face_count = 0
-contrast_reset = False
-miniscreen_contrast = 255
 
 
-def draw_top_text(canvas, text):
-    w_t, h_t = text_font.getsize(text)
-    confidence_text_pos = ((w_ms - w_t) // 2, 0)
-    canvas.text(confidence_text_pos, text, fill=1, font=text_font)
+def update_displayed_image(face, emotion):
+    def draw_text(canvas, text):
+        w_t, h_t = text_font.getsize(text)
+        confidence_text_pos = ((w_ms - w_t) // 2, 0)
+        canvas.text(confidence_text_pos, text, fill=1, font=text_font)
 
+    def draw_emoji(canvas, emoji, bounding_box, offset_y=2):
+        emoji_text = emoji_unicodes[emoji]
+        w_e, h_e = emoji_font.getsize(emoji_text)
+        x_b, y_b, w_b, h_b = bounding_box
+        emoji_text_pos = (x_b + (w_b - w_e) // 2, y_b + (h_b - h_e) // 2 - offset_y)
+        canvas.text(emoji_text_pos, emoji_text, fill=1, font=emoji_font)
 
-def draw_emoji(canvas, emoji, bounding_box, offset_y=2):
-    emoji_text = emoji_unicodes[emoji]
-    w_e, h_e = emoji_font.getsize(emoji_text)
-    x_b, y_b, w_b, h_b = bounding_box
-    emoji_text_pos = (x_b + (w_b - w_e) // 2, y_b + (h_b - h_e) // 2 - offset_y)
-    canvas.text(emoji_text_pos, emoji_text, fill=1, font=emoji_font)
+    def draw_facial_landmarks(canvas, face):
+        x_f, y_f, w_f, h_f = face.rectangle
+        x_l, y_l, w_l, h_l = left_bounding_box
+        translation_vector = [x_f - x_l + (w_l - w_f) // 2, y_f - y_l + (h_l - h_f) // 2]
 
+        scaler_width = w_l / w_f
+        scaler_height = h_l / h_f
+        scaler = scaler_width if scaler_width < scaler_height else scaler_height
+        scaler *= 0.8  # scale further as face rectangle is often smaller than face feature bounds
+        face_features = (face.features - translation_vector) * scaler
 
-def draw_emotion_data(canvas, emotion):
-    confidence_text = f"{round(emotion.confidence * 100)}% {emotion.type}"
-    draw_top_text(canvas, confidence_text)
-    draw_emoji(canvas, emotion.type, right_bounding_box)
-
-
-def draw_facial_landmarks(canvas, face):
-    x_f, y_f, w_f, h_f = face.rectangle
-    x_l, y_l, w_l, h_l = left_bounding_box
-
-    translation_vector = [x_f - x_l + (w_l - w_f) // 2, y_f - y_l + (h_l - h_f) // 2]
-
-    scaler_width = w_l / w_f
-    scaler_height = h_l / h_f
-    if scaler_width < scaler_height:
-        scaler = scaler_width
-    else:
-        scaler = scaler_height
-
-    scaler *= 0.8  # scale further by 0.8 as face rectangle often smaller than face feature bounds
-
-    face_features = (face.features - translation_vector) * scaler
-
-    for (x, y) in face_features:
-        canvas.point((x, y), fill=1)
-
-
-def found_face_display(canvas, face, emotion):
-    global looking_for_face_count, miniscreen_contrast, contrast_reset
-    looking_for_face_count = 0
-    miniscreen_contrast = 255
-    if not contrast_reset:
-        robot.miniscreen.contrast(miniscreen_contrast)
-        contrast_reset = True
-    draw_facial_landmarks(canvas=canvas, face=face)
-    draw_emotion_data(canvas=canvas, emotion=emotion)
-
-
-def looking_for_face_display(canvas):
-    global looking_for_face_count
-
-    if looking_for_face_count > 100:
-        global miniscreen_contrast, contrast_reset
-        miniscreen_contrast = max(0, miniscreen_contrast - 1)
-        robot.miniscreen.contrast(miniscreen_contrast)
-        contrast_reset = False
-
-    draw_emoji(canvas=canvas, emoji="Looking", bounding_box=wide_bounding_box, offset_y=5)
-
-    text = "Looking..."
-    draw_top_text(canvas=canvas, text=text)
-
-    looking_for_face_count += 1
-
-
-def frame_callback(frame):
-    face = face_detector(frame)
-    emotion = emotion_classifier(face)
+        for (x, y) in face_features:
+            canvas.point((x, y), fill=1)
 
     canvas = ImageDraw.Draw(image)
     canvas.rectangle(robot.miniscreen.bounding_box, fill=0)
 
     if face.found:
-        found_face_display(canvas, face, emotion)
+        draw_facial_landmarks(canvas=canvas, face=face)
+        draw_emoji(canvas, emotion.type, right_bounding_box)
+        draw_text(canvas, f"{round(emotion.confidence * 100)}% {emotion.type}")
     else:
-        looking_for_face_display(canvas)
+        draw_emoji(canvas=canvas, emoji="Looking", bounding_box=wide_bounding_box, offset_y=5)
+        draw_text(canvas=canvas, text="Looking...")
 
     robot.miniscreen.display_image(image)
+
+
+def run_emotion_and_face_detector(frame):
+    face = face_detector(frame)
+    emotion = emotion_classifier(face)
+    update_displayed_image(face, emotion)
 
 
 robot = Pitop()
@@ -138,6 +98,6 @@ wide_bounding_box = (0, top_section_height, w_ms, h_ms - top_section_height)
 face_detector = FaceDetector()
 emotion_classifier = EmotionClassifier()
 
-robot.camera.on_frame = frame_callback
+robot.camera.on_frame = run_emotion_and_face_detector
 
 pause()
