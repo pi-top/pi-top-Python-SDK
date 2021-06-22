9d5bd69fbe50752806b5da64fce9ddd470371a33
Ryan Dunwoody <me@ryandunwoody.com>
2021-06-22 18:41:43 +0800

    Add shake and nod functions

--- pitop/robotics/tilt_roll_head_controller.py
+++ pitop/robotics/tilt_roll_head_controller.py
@@ -5,6 +5,8 @@ from pitop.core.mixins import (
 from pitop.pma import ServoMotor
 from pitop.robotics.two_servo_assembly_calibrator import TwoServoAssemblyCalibrator
 from simple_pid import PID
+from threading import Thread, Event
+from time import sleep
 
 
 class TiltRollHeadController(Stateful, Recreatable):
@@ -25,21 +27,80 @@ class TiltRollHeadController(Stateful, Recreatable):
                                   )
 
         Stateful.__init__(self, children=['_roll_servo', '_tilt_servo'])
-        Recreatable.__init__(self, config_dict={'servo_roll_port': servo_roll_port, 'servo_tilt_port': servo_tilt_port, 'name': name})
+        Recreatable.__init__(self, config_dict={'servo_roll_port': servo_roll_port,
+                                                'servo_tilt_port': servo_tilt_port,
+                                                'name': name}
+                             )
 
     @property
-    def roll(self):
+    def roll(self) -> ServoMotor:
         return self._roll_servo
 
     @property
-    def tilt(self):
+    def tilt(self) -> ServoMotor:
         return self._tilt_servo
 
-    def stop(self):
+    def stop(self) -> None:
         self.tilt.sweep(speed=0)
         self.roll.sweep(speed=0)
 
-    def track_head_angle(self, angle, flipped=True):
+    def shake(self, times: int = 4, angle: int = 5, speed: int = 100, block: bool = True) -> None:
+        """
+        Shakes the head by rotating the roll servo back and forth between :data:`-angle` and :data:`+angle` from
+        current angle position.
+
+        :param int times: Number of times to shake head
+        :param int angle: angle in degrees either side of current angle to rotate servo by
+        :param int speed: servo speed in deg/s
+        :param bool block: if `True`, function call will block program execution until finished
+        :return: None
+        """
+        self.__start_servo_oscillation(servo=self._roll_servo, times=times, angle=angle, speed=speed, block=block)
+
+    def nod(self, times: int = 4, angle: int = 5, speed: int = 100, block: bool = True) -> None:
+        """
+        Nod the head by rotating the tilt servo back and forth between :data:`-angle` and :data:`+angle` from
+        current angle position.
+
+        :param int times: Number of times to nod head
+        :param int angle: angle in degrees either side of current angle to rotate servo by
+        :param int speed: servo speed in deg/s
+        :param bool block: if `True`, function call will block program execution until finished
+        :return: None
+        """
+        self.__start_servo_oscillation(servo=self._tilt_servo, times=times, angle=angle, speed=speed, block=block)
+
+    def __start_servo_oscillation(self, servo: ServoMotor, times: int, angle: int, speed: int, block: bool) -> None:
+        if block:
+            self.__oscillate_servo(servo=servo, times=times, angle=angle, speed=speed)
+        else:
+            Thread(target=self.__oscillate_servo,
+                   kwargs={"servo": servo, "times": times, "angle": angle, "speed": speed},
+                   daemon=True).start()
+
+    def __oscillate_servo(self, servo: ServoMotor, times: int, angle: int, speed: int) -> None:
+        previous_target_speed = servo.target_speed
+        servo.target_speed = speed
+
+        current_angle = servo.current_angle
+        shake_angle_start = current_angle - angle
+        shake_angle_end = current_angle + angle
+
+        for _ in range(times):
+            self.__set_angle_until_reached(servo=servo, angle=shake_angle_start)
+            self.__set_angle_until_reached(servo=servo, angle=shake_angle_end)
+
+        # Reset the servo state to how it was before
+        servo.target_angle = current_angle
+        servo.target_speed = previous_target_speed
+
+    @staticmethod
+    def __set_angle_until_reached(servo: ServoMotor, angle):
+        servo.target_angle = angle
+        while abs(angle - servo.current_angle) > 1:
+            sleep(0.05)
+
+    def track_head_angle(self, angle, flipped=True) -> None:
         if not flipped:
             angle = -angle
         current_angle = self.roll.current_angle
@@ -50,7 +111,7 @@ class TiltRollHeadController(Stateful, Recreatable):
             servo_speed = self._head_roll_pid(state)
             self.roll.sweep(speed=servo_speed)
 
-    def calibrate(self, save=True, reset=False):
+    def calibrate(self, save=True, reset=False) -> None:
         """Calibrates the assembly to work in optimal conditions.
 
         Based on the provided arguments, it will either load the calibration
