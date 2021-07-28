c45cfb22b094baa6ad5fe76006e47b44f4767fe7
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2021-07-28 17:36:56 +0100

    Refactor attempt

--- pitop/labs/web/blueprints/video/__init__.py
+++ pitop/labs/web/blueprints/video/__init__.py
@@ -3,30 +3,31 @@ from io import BytesIO
 from flask import Response, abort, Blueprint
 
 
+def generate_frames(get_frame_func):
+    def _get_frame():
+        try:
+            frame = get_frame_func()
+            buffered = BytesIO()
+            frame.save(buffered, format="JPEG")
+            return buffered.getvalue()
+        except Exception as e:
+            print(e)
+
+    pool = gevent.get_hub().threadpool
+    while True:
+        # get_frame in thread so it won't block handler greenlets
+        frame_bytes = pool.spawn(get_frame_func).get()
+        yield (
+            b'--frame\r\n'
+            b'Content-Type: image/jpeg\r\n\r\n'+frame_bytes+b'\r\n'
+        )
+
+
 class VideoResponse(Response):
     def __init__(self, get_frame=None, *args, **kwargs):
-        def __generate_frames():
-            def __get_frame():
-                try:
-                    frame = get_frame()
-                    buffered = BytesIO()
-                    frame.save(buffered, format="JPEG")
-                    return buffered.getvalue()
-                except Exception as e:
-                    print(e)
-
-            pool = gevent.get_hub().threadpool
-            while True:
-                # get_frame in thread so it won't block handler greenlets
-                frame_bytes = pool.spawn(__get_frame).get()
-                yield (
-                    b'--frame\r\n'
-                    b'Content-Type: image/jpeg\r\n\r\n'+frame_bytes+b'\r\n'
-                )
-
-        super(VideoResponse, self).__init__(
+        Response.__init__(
             self,
-            __generate_frames(),
+            generate_frames(get_frame),
             mimetype='multipart/x-mixed-replace; boundary=frame',
             **kwargs
         )
