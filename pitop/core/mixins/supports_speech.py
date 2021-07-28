25ba45a0a57afc7c0d3de1c9d079452f250903fc
Ryan Dunwoody <me@ryandunwoody.com>
2021-07-28 15:59:41 +0800

    Remove device check
    Any raspberry pi with speaker connected will work

--- pitop/core/mixins/supports_speech.py
+++ pitop/core/mixins/supports_speech.py
@@ -1,19 +1,8 @@
-from pitop.core.exceptions import UnavailableComponent
-from pitop.system import device_type
-
-from pitopcommon.common_names import DeviceName
-
-
 class SupportsSpeech:
     def __init__(self):
         from pitop.processing.speech import TTS
-        self._tts = None
-        if device_type() == DeviceName.pi_top_4.value:
-            # TODO: probably unnecessary, can work on any device with a speaker
-            self._tts = TTS()
+        self._tts = TTS()
 
     @property
     def speak(self):
-        if self._tts:
-            return self._tts
-        raise UnavailableComponent("Speech isn't available on this device")
+        return self._tts
