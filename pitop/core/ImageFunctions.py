0f1090f6acca45b096c2e0f25eeb318924cb1cbf
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2021-08-05 19:43:34 +0100

    Only import OpenCV for converting if it's needed

--- pitop/core/ImageFunctions.py
+++ pitop/core/ImageFunctions.py
@@ -14,17 +14,6 @@ def image_format_check(format):
 
 
 def convert(image, format="PIL"):
-
-    try:
-        from cv2 import (
-            cvtColor,
-            COLOR_BGR2RGB,
-            COLOR_RGB2BGR,
-        )
-    except (ImportError, ModuleNotFoundError):
-        raise ModuleNotFoundError(
-            "OpenCV Python library is not installed. You can install it by running 'sudo apt install python3-opencv libatlas-base-dev'.") from None
-
     image_format_check(format)
     format = format.lower()
 
@@ -34,18 +23,31 @@ def convert(image, format="PIL"):
         isinstance(image, ndarray) and format == "opencv"
     ]):
         return image
-    elif isinstance(image, Image.Image) and format == "opencv":
-        # Convert PIL to OpenCV
-        cv_image = asarray(image)
-        if image.mode == "RGB":
-            cv_image = cvtColor(cv_image, COLOR_RGB2BGR)
-        return cv_image
-    elif isinstance(image, ndarray) and format == "pil":
-        # Convert OpenCV to PIL
-        if len(image.shape) > 2 and image.shape[2] == 3:
-            # If incoming image has 3 channels, convert from BGR to RGB
-            image = cvtColor(image, COLOR_BGR2RGB)
-        return Image.fromarray(image)
+    else:
+
+        try:
+            from cv2 import (
+                cvtColor,
+                COLOR_BGR2RGB,
+                COLOR_RGB2BGR,
+            )
+        except (ImportError, ModuleNotFoundError):
+            raise ModuleNotFoundError(
+                "OpenCV Python library is not installed. You can install it by running 'sudo apt install python3-opencv libatlas-base-dev'."
+            ) from None
+
+        if isinstance(image, Image.Image) and format == "opencv":
+            # Convert PIL to OpenCV
+            cv_image = asarray(image)
+            if image.mode == "RGB":
+                cv_image = cvtColor(cv_image, COLOR_RGB2BGR)
+            return cv_image
+        elif isinstance(image, ndarray) and format == "pil":
+            # Convert OpenCV to PIL
+            if len(image.shape) > 2 and image.shape[2] == 3:
+                # If incoming image has 3 channels, convert from BGR to RGB
+                image = cvtColor(image, COLOR_BGR2RGB)
+            return Image.fromarray(image)
 
 
 def get_pil_image_from_path(file_path_or_url):
