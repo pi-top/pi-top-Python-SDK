4636cf13a70b6c78709951224c446daff68af333
Ryan Dunwoody <me@ryandunwoody.com>
2021-08-06 16:28:35 +0800

    Fix comments

--- examples/recipes/prax_shake_head.py
+++ examples/recipes/prax_shake_head.py
@@ -10,11 +10,11 @@ head.tilt.target_angle = 50
 sleep(1)
 
 
-# Nod 6 times at max speed across a 5 degree angle. Block program execution until finished.
+# Nod 6 times at max speed 5 degrees either side of current angle. Blocks program execution until finished.
 head.nod(times=6, angle=5, speed=100, block=True)
 
-# Shake 6 times at half speed across a 10 degree angle. Block program execution until finished.
-head.shake(times=6, angle=10, speed=50, block=True)
+# Shake 4 times at half speed 10 degrees either side of current angle. Blocks program execution until finished.
+head.shake(times=4, angle=10, speed=50, block=True)
 
 # Shake and nod at the same time with default speed and angle
 # Setting nod with block=False ensures the program continues to the next command
