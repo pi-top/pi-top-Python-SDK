a5b5a795defb9adb244960240dbefc1532d4f66e
Jorge Capona <jcapona@gmail.com>
2021-08-09 16:48:34 -0400

    Return build information as an object

--- pitop/common/pt_os.py
+++ pitop/common/pt_os.py
@@ -1,8 +1,42 @@
+from dataclasses import dataclass, asdict
 from os import path
 from getpass import getuser
 
+
 ptissue_path = "/boot/pt-issue.txt"
 legacy_ptissue_path = "/etc/pt-issue"
+
+
+@dataclass
+class PitopOsBuildInfoV1:
+    schema_version: str
+    build_type: str
+    date: str
+    build_time_os_version: str
+    build_run_number: str
+    commit: str
+
+    @classmethod
+    def from_os_build_info(cls, build_info):
+        DELIMITER: str = "_"
+        LOOKUP = {
+            "S": "schema_version",
+            "B": "build_type",
+            "D": "date",
+            "P": "build_time_os_version",
+            "R": "build_run_number",
+            "#": "commit",
+        }
+        build_info_fields = build_info.split(DELIMITER)
+        build_info_dict = {LOOKUP.get(field[0]): field[1:] for field in build_info_fields}
+        return cls(**build_info_dict)
+
+    @property
+    def name(self):
+        return f"S{self.schema_version}_B_{self.build_type}_D{self.date}_P{self.build_time_os_version}_#{self.commit}_R{self.build_run_number}"
+
+    def as_dict(self):
+        return asdict(self)
 
 
 def is_pi_top_os():
@@ -23,7 +57,11 @@ def get_pitopOS_info():
     if not path.isfile(ptissue_path):
         return
 
-    return _parse_ptissue(ptissue_path)
+    try:
+        build_info = _parse_ptissue(ptissue_path)
+        return PitopOsBuildInfoV1.from_os_build_info(build_info.get("Build ID"))
+    except Exception:
+        raise Exception("Couldn't detect a valid build ID schema")
 
 
 def get_legacy_pitopOS_info():
