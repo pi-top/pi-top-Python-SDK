cdb1199ac148c9ec2e5baf0795baa5a4c196942a
Jorge Capona <jcapona@gmail.com>
2021-08-10 12:16:59 -0400

    Refactor

--- pitop/common/pt_os.py
+++ pitop/common/pt_os.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass, asdict
+from dataclasses import dataclass
 from os import path
 from getpass import getuser
 
@@ -8,67 +8,72 @@ legacy_ptissue_path = "/etc/pt-issue"
 
 
 @dataclass
-class PitopOsBuildInfoV1:
-    schema_version: str
-    build_type: str
-    date: str
-    build_time_os_version: str
+class PitopOsBuildInfo:
+    build_date: str
     build_run_number: str
-    commit: str
+    build_commit: str
+    # latest fields
+    schema_version: str = ""
+    build_type: str = ""
+    build_os_version: str = ""
+    # legacy fields
+    build_name: str = ""
+    build_repo: str = ""
+    final_repo: str = ""
 
-    @classmethod
-    def from_os_build_info(cls, build_info):
+
+def is_pi_top_os():
+    return path.isfile(ptissue_path) or path.isfile(legacy_ptissue_path)
+
+
+def get_pitopOS_info():
+    def file_to_dict(path_to_file):
+        data = {}
+        with open(path_to_file, 'r') as reader:
+            for line in reader.readlines():
+                content = line.split(":")
+                if len(content) == 2:
+                    data[content[0].strip()] = content[1].strip()
+        return data
+
+    def parse_ptissue(build_info_path):
         DELIMITER: str = "_"
         LOOKUP = {
             "S": "schema_version",
             "B": "build_type",
-            "D": "date",
-            "P": "build_time_os_version",
+            "D": "build_date",
+            "P": "build_os_version",
             "R": "build_run_number",
-            "#": "commit",
+            "#": "build_commit",
         }
-        build_info_fields = build_info.split(DELIMITER)
-        build_info_dict = {LOOKUP.get(field[0]): field[1:] for field in build_info_fields}
-        return cls(**build_info_dict)
+        build_info_file_dict = file_to_dict(build_info_path)
+        build_id = build_info_file_dict.get("Build ID")
+        build_info_fields = build_id.split(DELIMITER)
+        return {LOOKUP.get(field[0]): field[1:] for field in build_info_fields}
 
-    @property
-    def name(self):
-        return f"S{self.schema_version}_B_{self.build_type}_D{self.date}_P{self.build_time_os_version}_#{self.commit}_R{self.build_run_number}"
+    def parse_legacy_ptissue(build_info_path):
+        LOOKUP = {
+            "Build Name": "build_name",
+            "Build Date": "build_date",
+            "Build Number": "build_run_number",
+            "Build Pipeline Commit Hash": "build_commit",
+            "Build Apt Repo": "build_repo",
+            "Final Apt Repo": "final_repo",
+        }
+        build_info_file_dict = file_to_dict(build_info_path)
+        return {LOOKUP.get(field): value for field, value in build_info_file_dict.items()}
 
-    def as_dict(self):
-        return asdict(self)
-
-
-def is_pi_top_os():
-    return path.isfile(ptissue_path)
-
-
-def _parse_ptissue(ptissue_path):
-    data = {}
-    with open(ptissue_path, 'r') as reader:
-        for line in reader.readlines():
-            content = line.split(":")
-            if len(content) == 2:
-                data[content[0].strip()] = content[1].strip()
-    return data
-
-
-def get_pitopOS_info():
-    if not path.isfile(ptissue_path):
-        return
+    if path.isfile(ptissue_path):
+        build_info_dict = parse_ptissue(ptissue_path)
+    elif path.isfile(legacy_ptissue_path):
+        build_info_dict = parse_legacy_ptissue(legacy_ptissue_path)
+    else:
+        return None
 
     try:
-        build_info = _parse_ptissue(ptissue_path)
-        return PitopOsBuildInfoV1.from_os_build_info(build_info.get("Build ID"))
+        return PitopOsBuildInfo(**build_info_dict)
     except Exception:
-        raise Exception("Couldn't detect a valid build ID schema")
-
-
-def get_legacy_pitopOS_info():
-    if not path.isfile(legacy_ptissue_path):
-        return
-
-    return _parse_ptissue(legacy_ptissue_path)
+        pass
 
 
 def is_pi_using_default_password():
