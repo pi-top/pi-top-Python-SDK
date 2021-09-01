1de2ad3f5a642ee0b63c108e0de617800f9a543f
lintian-brush <noreply@github.com>
2021-09-01 14:41:07 +0100

    Fix version.py

--- pitop/version.py
+++ pitop/version.py
@@ -1,7 +1,10 @@
-__version__ = "N/A"
+import os
 
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+__version__ = "N/A"
 try:
-    with open("version.txt", "r+") as f:
+    with open(f"{HERE}/version.txt", "r+") as f:
         # Reading form a file
         __version__ = f.read().strip()
 
