2f2ae44b6d19b465da022f12f6817f6dd96c86a3
lintian-brush <noreply@github.com>
2021-09-01 14:18:12 +0100

    Use version file

--- pitop/version.py
+++ pitop/version.py
@@ -1,1 +1,9 @@
-__version__ = "0.19.1"
+__version__ = "N/A"
+
+try:
+    with open("version.txt", "r+") as f:
+        # Reading form a file
+        __version__ = f.read().strip()
+
+except Exception:
+    pass
