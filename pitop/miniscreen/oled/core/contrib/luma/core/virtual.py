bbf7953f1d3dc53fe44b94915a9ab287dee9d29e
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2021-09-10 16:28:46 +0100

    Update virtual.py (#458)

--- pitop/miniscreen/oled/core/contrib/luma/core/virtual.py
+++ pitop/miniscreen/oled/core/contrib/luma/core/virtual.py
@@ -1,8 +1,130 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2017-2020 Richard Hull and contributors
+# See LICENSE.rst for details.
+
 from time import perf_counter
 
 from PIL import Image, ImageDraw
 
 from . import mixin
+from .threadpool import threadpool
+
+pool = threadpool(4)
+
+
+def calc_bounds(xy, entity):
+    """For an entity with width and height attributes, determine the bounding
+    box if were positioned at ``(x, y)``."""
+    left, top = xy
+    right, bottom = left + entity.width, top + entity.height
+    return [left, top, right, bottom]
+
+
+def range_overlap(a_min, a_max, b_min, b_max):
+    """Neither range is completely greater than the other."""
+    return (a_min < b_max) and (b_min < a_max)
+
+
+class viewport(mixin.capabilities):
+    """The viewport offers a positionable window into a larger resolution
+    pseudo-display, that also supports the concept of hotspots (which act like
+    live displays).
+
+    :param device: The device to project the enlarged pseudo-display viewport onto.
+    :param width: The number of horizontal pixels.
+    :type width: int
+    :param height: The number of vertical pixels.
+    :type height: int
+    :param mode: The supported color model, one of ``"1"``, ``"RGB"`` or
+        ``"RGBA"`` only.
+    :type mode: str
+    :param dither: By default, any color (other than black) will be `generally`
+        treated as white when displayed on monochrome devices. However, this behaviour
+        can be changed by adding ``dither=True`` and the image will be converted from RGB
+        space into a 1-bit monochrome image where dithering is employed to differentiate
+        colors at the expense of resolution.
+    :type dither: bool
+    """
+
+    def __init__(self, device, width, height, mode=None, dither=False):
+        self.capabilities(width, height, rotate=0, mode=mode or device.mode)
+        if hasattr(device, "segment_mapper"):
+            self.segment_mapper = device.segment_mapper
+        self._device = device
+        self._backing_image = Image.new(self.mode, self.size)
+        self._position = (0, 0)
+        self._hotspots = []
+        self._dither = dither
+
+    def display(self, image):
+        assert image.mode == self.mode
+        assert image.size == self.size
+
+        self._backing_image.paste(image)
+        self.refresh()
+
+    def set_position(self, xy):
+        self._position = xy
+        self.refresh()
+
+    def add_hotspot(self, hotspot, xy):
+        """Add the hotspot at ``(x, y)``.
+
+        The hotspot must fit inside the bounds of the virtual device. If
+        it does not then an ``AssertError`` is raised.
+        """
+        (x, y) = xy
+        assert 0 <= x <= self.width - hotspot.width
+        assert 0 <= y <= self.height - hotspot.height
+
+        # TODO: should it check to see whether hotspots overlap each other?
+        # Is sensible to _allow_ them to overlap?
+        self._hotspots.append((hotspot, xy))
+
+    def remove_hotspot(self, hotspot, xy):
+        """Remove the hotspot at ``(x, y)``: Any previously rendered image
+        where the hotspot was placed is erased from the backing image, and will
+        be "undrawn" the next time the virtual device is refreshed.
+
+        If the specified hotspot is not found for ``(x, y)``, a
+        ``ValueError`` is raised.
+        """
+        self._hotspots.remove((hotspot, xy))
+        eraser = Image.new(self.mode, hotspot.size)
+        self._backing_image.paste(eraser, xy)
+
+    def is_overlapping_viewport(self, hotspot, xy):
+        """Checks to see if the hotspot at position ``(x, y)`` is (at least
+        partially) visible according to the position of the viewport."""
+        l1, t1, r1, b1 = calc_bounds(xy, hotspot)
+        l2, t2, r2, b2 = calc_bounds(self._position, self._device)
+        return range_overlap(l1, r1, l2, r2) and range_overlap(t1, b1, t2, b2)
+
+    def refresh(self):
+        should_wait = False
+        for hotspot, xy in self._hotspots:
+            if hotspot.should_redraw() and self.is_overlapping_viewport(hotspot, xy):
+                pool.add_task(hotspot.paste_into, self._backing_image, xy)
+                should_wait = True
+
+        if should_wait:
+            pool.wait_completion()
+
+        im = self._backing_image.crop(box=self._crop_box())
+        if self._dither:
+            im = im.convert(self._device.mode)
+
+        self._device.display(im)
+
+    def _crop_box(self):
+        (left, top) = self._position
+        right = left + self._device.width
+        bottom = top + self._device.height
+
+        assert 0 <= left <= right <= self.width
+        assert 0 <= top <= bottom <= self.height
+
+        return (left, top, right, bottom)
 
 
 class hotspot(mixin.capabilities):
