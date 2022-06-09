63522202a6aa556112c8bf4b3e455c24815d294e
Jorge Capona <jcapona@gmail.com>
2022-06-09 16:05:29 -0400

    Mark ptlock test as flaky

--- conftest.py
+++ conftest.py
@@ -1,9 +1,6 @@
-import gc
 from os import environ
 from sys import modules
 from unittest.mock import Mock
-
-import pytest
 
 for module in [
     "pitop.common.ptdm.zmq",
@@ -18,8 +15,3 @@ for module in [
 
 # use gpiozero fake pins
 environ["GPIOZERO_PIN_FACTORY"] = "mock"
-
-
-@pytest.fixture(autouse=True)
-def ensure_gc():
-    gc.collect()
