9f318fecc570eb03eda14abe045d8a554b9905d6
Jorge Capona <jcapona@gmail.com>
2022-08-17 15:45:38 -0400

    Set package version using environment variable

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
-"""pi-top Python SDK."""
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from os import environ
+
 import setuptools
 
 if __name__ == "__main__":
-    setuptools.setup()
+    setuptools.setup(
+        version=environ.get("PYTHON_PACKAGE_VERSION", "0.0.1.dev1").replace('"', "")
+    )
