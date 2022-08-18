db74bdf12f3c9c59545c60bc958b08bab0a6185d
Jorge Capona <jcapona@users.noreply.github.com>
2022-08-18 15:53:09 -0400

    Set package version using environment variable (#551)

--- pitop/version.py
+++ pitop/version.py
@@ -1,12 +1,7 @@
-import os
-
-HERE = os.path.abspath(os.path.dirname(__file__))
+from pkg_resources import get_distribution
 
 __version__ = "N/A"
 try:
-    with open(f"{HERE}/version.txt", "r+") as f:
-        # Reading form a file
-        __version__ = f.read().strip()
-
+    __version__ = get_distribution("pitop").version
 except Exception:
     pass
