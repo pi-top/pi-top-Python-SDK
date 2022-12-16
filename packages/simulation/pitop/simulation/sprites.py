7064eaccb965b967f881de216849639f76a3cbef
Jorge Capona <jcapona@gmail.com>
2022-12-16 12:31:17 -0500

    Draw port label on componentable simsprites

--- packages/simulation/pitop/simulation/sprites.py
+++ packages/simulation/pitop/simulation/sprites.py
@@ -6,6 +6,7 @@ import pygame
 from . import images as Images
 from .events import SimEvent, SimEventTypes
 from .images import PMA_CUBE_SIZE
+from .port_label import PortLabel
 from .simsprite import ComponentableSimSprite, SimSprite
 from .utils import multiply_scalar
 from .virtual_hardware import is_virtual_hardware
@@ -13,15 +14,15 @@ from .widgets.slider import Slider
 
 MARGIN = 10
 SLIDER_WIDTH = 85
-SLIDER_HORIZONTAL_OFFSET = int((PMA_CUBE_SIZE[0] - SLIDER_WIDTH) / 2)
 SLIDER_HEIGHT = 8
 SLIDER_HANDLE_RADIUS = 8
 FONT_SIZE = 22
 TEXT_OFFSET = (-13, 8)
+PITOP_GRAY_COLOR = pygame.Color("#505050")
 
 
 class Pitop(pygame.sprite.Sprite, ComponentableSimSprite):
-    def __init__(self, config, scale):
+    def __init__(self, config, scale, draw_port=False):
         pygame.sprite.Sprite.__init__(self)
 
         self.scale = scale
@@ -46,7 +47,7 @@ class Pitop(pygame.sprite.Sprite, ComponentableSimSprite):
 
 
 class LED(pygame.sprite.Sprite, SimSprite):
-    def __init__(self, config, scale):
+    def __init__(self, config, scale, draw_port=False):
         pygame.sprite.Sprite.__init__(self)
 
         self.color = config.get("color", "red")
@@ -56,6 +57,17 @@ class LED(pygame.sprite.Sprite, SimSprite):
         self.on_image = SimSprite._load_image(
             getattr(Images, f"LED_{self.color}_on"), scale
         )
+
+        if draw_port:
+            self.Size = (self.Size[0], self.Size[1] + PortLabel.height + MARGIN)
+            port_label = PortLabel(
+                config.get("port_name"),
+                self.Size,
+                scale,
+                pos=(int(self.Size[0]) / 2 - 40, PortLabel.height),
+            )
+            self.on_image = port_label.render(self.on_image)
+            self.off_image = port_label.render(self.off_image)
 
         self.image = self.off_image
         self.rect = self.image.get_rect()
@@ -71,11 +83,22 @@ class LED(pygame.sprite.Sprite, SimSprite):
 
 
 class Button(pygame.sprite.Sprite, SimSprite):
-    def __init__(self, config, scale):
+    def __init__(self, config, scale, draw_port=False):
         pygame.sprite.Sprite.__init__(self)
 
         self.released_image = SimSprite._load_image(Images.Button, scale)
         self.pressed_image = SimSprite._load_image(Images.Button_pressed, scale)
+
+        if draw_port:
+            self.Size = (self.Size[0], self.Size[1] + PortLabel.height + MARGIN)
+            port_label = PortLabel(
+                config.get("port_name"),
+                self.Size,
+                scale,
+                pos=(int(self.Size[0]) / 2 - 40, PortLabel.height),
+            )
+            self.released_image = port_label.render(self.released_image)
+            self.pressed_image = port_label.render(self.pressed_image)
 
         self.image = self.released_image
         self.rect = self.image.get_rect()
@@ -103,9 +126,9 @@ class Button(pygame.sprite.Sprite, SimSprite):
 
 
 class Buzzer(pygame.sprite.Sprite, SimSprite):
-    Size = ((PMA_CUBE_SIZE[0] + 2 * MARGIN) * 2, PMA_CUBE_SIZE[0] + 2 * MARGIN)
+    Size = ((PMA_CUBE_SIZE[0]) * 2, PMA_CUBE_SIZE[0])
 
-    def __init__(self, config, scale):
+    def __init__(self, config, scale, draw_port=False):
         pygame.sprite.Sprite.__init__(self)
 
         def get_pos(image):
@@ -122,6 +145,26 @@ class Buzzer(pygame.sprite.Sprite, SimSprite):
 
         self.base_image = pygame.Surface(multiply_scalar(scale, self.Size))
         self.base_image.fill(pygame.Color("WHITE"))
+
+        if draw_port:
+            label_height = PortLabel.height + MARGIN
+            self.Size = (self.Size[0], self.Size[1] + label_height)
+            port_label = PortLabel(
+                config.get("port_name"),
+                self.Size,
+                scale,
+                pos=(int(self.Size[0]) / 2 - 30, PortLabel.height),
+            )
+            self.base_image = port_label.render(self.base_image)
+            self.component_pos = (
+                self.component_pos[0],
+                self.component_pos[1] + scale * label_height,
+            )
+            self.buzzer_sound_image_pos = (
+                self.buzzer_sound_image_pos[0],
+                self.buzzer_sound_image_pos[1] + scale * label_height,
+            )
+
         self.rect = self.base_image.get_rect()
 
     def render(self):
@@ -134,6 +177,7 @@ class Buzzer(pygame.sprite.Sprite, SimSprite):
             image.blit(self.buzzer_sound_image, self.buzzer_sound_image_pos)
 
         image.blit(self.component_image, self.component_pos)
+
         return image
 
     def update(self):
@@ -153,6 +197,7 @@ class SliderSensorSprite(pygame.sprite.Sprite, SimSprite):
     slider: Slider
     font: Component
     icon: Component
+    img: Component
     scale: float
 
     image: str
@@ -162,15 +207,20 @@ class SliderSensorSprite(pygame.sprite.Sprite, SimSprite):
     max_value: int = 999
     slider_step: int = 1
 
-    def __init__(self, config, scale):
+    def __init__(self, config, scale, draw_port=False):
         pygame.sprite.Sprite.__init__(self)
         self.scale = scale
 
-        self.create_components()
+        self.port_label = None
+        if draw_port:
+            self.Size = (self.Size[0], self.Size[1] + PortLabel.height + MARGIN)
+            self.port_label = PortLabel(config.get("port_name"), self.Size, scale)
 
+        self.create_components()
         assert self.slider is not None
         assert self.font is not None
         assert self.icon is not None
+        assert self.img is not None
 
         self.base_image = self.draw_base_image()
 
@@ -181,11 +231,14 @@ class SliderSensorSprite(pygame.sprite.Sprite, SimSprite):
         self.rect = self.image.get_rect()
 
     def create_components(self):
-        right_hand_side_x_start = self.Size[0] - MARGIN - PMA_CUBE_SIZE[0]
+        label_height = (
+            self.scale * (PortLabel.height + MARGIN) if self.port_label else 0
+        )
+        right_hand_side_x_start = self.Size[0] - SLIDER_WIDTH - SLIDER_HANDLE_RADIUS - 1
 
         slider_pos = (
-            right_hand_side_x_start + SLIDER_HORIZONTAL_OFFSET,
-            self.Size[1] - 2 * MARGIN - SLIDER_HEIGHT,
+            right_hand_side_x_start,
+            self.Size[1] - MARGIN - SLIDER_HEIGHT,
         )
         slider_pos = multiply_scalar(self.scale, slider_pos)
 
@@ -199,7 +252,7 @@ class SliderSensorSprite(pygame.sprite.Sprite, SimSprite):
             step=self.slider_step,
             initial=0,
             handleRadius=int(SLIDER_HANDLE_RADIUS * self.scale),
-            handleColour=pygame.Color("#505050"),
+            handleColour=PITOP_GRAY_COLOR,
             colour=pygame.Color("#DBDEE0"),
         )
 
@@ -215,17 +268,27 @@ class SliderSensorSprite(pygame.sprite.Sprite, SimSprite):
         icon_image = SimSprite._load_image(self.icon, self.scale)
         icon_pos = (
             slider_pos[0] + self.slider.width / 2 - icon_image.get_width() / 2,
-            text_pos[1] - icon_image.get_height() - self.scale * MARGIN / 2,
+            text_pos[1]
+            - icon_image.get_height()
+            - self.scale * MARGIN / 2
+            - label_height,
         )
         self.icon = Component(icon_pos, icon_image)
 
-    def draw_base_image(self):
-        component_image = SimSprite._load_image(self.image, self.scale)
+        self.img = Component(
+            (0, 0),
+            SimSprite._load_image(self.image, self.scale),
+        )
 
+    def draw_base_image(self):
         base_image = pygame.Surface(multiply_scalar(self.scale, self.Size))
         base_image.fill(pygame.Color("WHITE"))
-        base_image.blit(component_image, multiply_scalar(self.scale, (MARGIN, MARGIN)))
+        base_image.blit(self.img.obj, self.img.pos)
         base_image.blit(self.icon.obj, self.icon.pos)
+
+        if self.port_label:
+            base_image = self.port_label.render(base_image)
+
         return base_image
 
     def set_pos(self, x, y):
@@ -240,7 +303,7 @@ class SliderSensorSprite(pygame.sprite.Sprite, SimSprite):
 
         image = self.base_image.copy()
         image.blit(
-            self.font.obj.render(str(value), True, pygame.Color("#505050")),
+            self.font.obj.render(str(value), True, PITOP_GRAY_COLOR),
             self.font.pos,
         )
 
@@ -248,6 +311,7 @@ class SliderSensorSprite(pygame.sprite.Sprite, SimSprite):
             self.slider.value = value
 
         self.slider.draw(image)
+
         return image
 
     def update(self):
@@ -273,14 +337,14 @@ class SliderSensorSprite(pygame.sprite.Sprite, SimSprite):
 
 
 class LightSensor(SliderSensorSprite):
-    Size = (PMA_CUBE_SIZE[0] * 2 + MARGIN * 3, PMA_CUBE_SIZE[1] + 2 * MARGIN)
+    Size = (PMA_CUBE_SIZE[0] * 2 + MARGIN, PMA_CUBE_SIZE[1])
     image = Images.LightSensor
     measurement_key_in_state = "reading"
     icon = Images.lightbulb_icon
 
 
 class SoundSensor(SliderSensorSprite):
-    Size = (PMA_CUBE_SIZE[0] * 2 + MARGIN * 3, PMA_CUBE_SIZE[1] + 2 * MARGIN)
+    Size = (PMA_CUBE_SIZE[0] * 2 + MARGIN, PMA_CUBE_SIZE[1])
     image = Images.SoundSensor
     max_value = 500
     measurement_key_in_state = "reading"
@@ -302,14 +366,14 @@ class SoundSensor(SliderSensorSprite):
 
 
 class Potentiometer(SliderSensorSprite):
-    Size = (PMA_CUBE_SIZE[0] * 2 + MARGIN * 3, PMA_CUBE_SIZE[1] + 2 * MARGIN)
+    Size = (PMA_CUBE_SIZE[0] * 2 + MARGIN, PMA_CUBE_SIZE[1])
     image = Images.Potentiometer
     measurement_key_in_state = "position"
     icon = Images.angle_icon
 
 
 class UltrasonicSensor(SliderSensorSprite):
-    Size = (PMA_CUBE_SIZE[0] * 3 + MARGIN * 3, PMA_CUBE_SIZE[1] + 2 * MARGIN)
+    Size = (PMA_CUBE_SIZE[0] * 3 + MARGIN, PMA_CUBE_SIZE[1])
     image = Images.UltrasonicSensor
     max_value = 3
     measurement_key_in_state = "distance"
