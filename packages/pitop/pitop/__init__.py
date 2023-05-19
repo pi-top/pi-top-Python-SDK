9b6cc9b194e17c9af4c0b8c167e4463d42a79234
Angus Whitehead <angusjfw@gmail.com>
2023-05-19 10:44:35 +0100

    Add additional exports to the pitop and robotics modules
    These exports follow the existing pattern of exporting all the key
    interfaces from both their subpackage and the top-level package.
    
    In particular this aims to fix the import of DriveController from
    pitop.robotics used the web-portal rover controller, which assumed this
    would be available when switching to use subpackages.

--- packages/pitop/pitop/__init__.py
+++ packages/pitop/pitop/__init__.py
@@ -4,8 +4,11 @@ __path__ = __import__("pkgutil").extend_path(__path__, __name__)
 import pitop.simulation
 
 # System Devices
+from pitop.battery import Battery
 from pitop.camera import Camera
+from pitop.display import Display
 from pitop.keyboard import KeyboardButton
+from pitop.miniscreen import Miniscreen
 
 # PMA
 from pitop.pma import (
@@ -22,11 +25,11 @@ from pitop.pma import ServoMotorSetting
 from pitop.pma import ServoMotorSetting as ServoMotorState
 from pitop.pma import SoundSensor, UltrasonicSensor
 from pitop.pma.parameters import BrakingType, Direction, ForwardDirection
+
+# Robotics
 from pitop.robotics.blockpi_rover import BlockPiRover
 from pitop.robotics.configurations import AlexRobot  # deprecated
 from pitop.robotics.configurations import alex_config
-
-# Robotics
 from pitop.robotics.drive_controller import DriveController
 from pitop.robotics.navigation import NavigationController
 from pitop.robotics.pan_tilt_controller import PanTiltController
